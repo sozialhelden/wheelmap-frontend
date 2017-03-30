98d2192e8ee7109321482f56082cd7d7b1e83ada
leekspin <git@slimegirl.net>
2017-03-30 22:54:21 +0200

    Add CategoryMenu Component
    and toggle button

---
+++ src/components/Categories.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { humanize } from 'underscore.string';
+import styled from 'styled-components';
+
+const categories = [
+  'education',
+  'accommodation',
+  'food',
+  'government',
+  'health',
+  'leisure',
+  'misc',
+  'money_post',
+  'public_transfer',
+  'shopping',
+  'sport',
+  'tourism',
+  'unknown',
+];
+
+export class Category extends Component {
+  render() {
+    const name = this.props.name;
+    const humanizedName = humanize(name);
+    const url = `categories/${name}`;
+
+    return (
+      <StyledLink to={url}>{humanizedName}</StyledLink>
+    );
+  }
+}
+
+const StyledLink= styled(Link)`
+  display: inline-flex;
+  flex-basis: 50%;
+  margin: 10px 0 10px 0;
+  font-size: 20px;
+  color: black;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  flex-flow: row wrap;
+  display: flex;
+  z-index: 1000;
+  top: 0px;
+  left: 0px;
+  width: 400px;
+  padding: 20px;
+  color: black;
+  font-size: 20px;
+  background-color: white;
+`;
+
+export class CategoryMenu extends Component {
+  render() {
+    return (
+      <Container>
+        {categories.map(name => <Category name={name} key={name}/>)}
+      </Container>
+    );
+  }
+}
+
+const StyledButton = styled.button`
+  margin: 20px;
+`;
+
+export const CategoryButton = (props) => (
+  <StyledButton onClick={props.toggleCategoryMenu}>
+    Categories
+  </StyledButton>
+);
+
+CategoryButton.propTypes = {
+  toggleCategoryMenu: React.PropTypes.func,
+}
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  CategoryButton,
+  CategoryMenu,
+} from './Categories';
 
 export default class Toolbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoryMenuIsVisible: false,
+    };
+    this.toggleCategoryMenu = this.toggleCategoryMenu.bind(this);
+  }
+
+  toggleCategoryMenu() {
+    this.setState(prevState => ({
+      categoryMenuIsVisible: !prevState.categoryMenuIsVisible,
+    }));
+  }
+
   render() {
+    let categoryMenu = null;
+
+    if (this.state.categoryMenuIsVisible) {
+      categoryMenu = <CategoryMenu />;
+    }
+
     return (
       <nav>
+        {categoryMenu}
+        <CategoryButton toggleCategoryMenu={this.toggleCategoryMenu} />
         <Link to="/">Wheelmap</Link>
         <Link to="/about">About</Link>
         <Link to="/topics">Topics</Link>
       </nav>
-    )
+    );
   }
 }
