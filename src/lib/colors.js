5dbc74204f6c5423aa5ba18486ac0ef3c943782f
Sebastian Felix Zappe <hypo@me.com>
2017-03-30 23:18:10 +0200

    Add d3-colors, update yarn.lock

---
+++ src/lib/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primaryColor: '#435D75',
+  secondaryColor: '#8B6A43',
+  highlightColor: '#435D75',
+  colorizedBackgroundColor: '#F2F1ED',
+  neutralBackgroundColor: '#F5F5F5',
+};
+
+export default colors;
