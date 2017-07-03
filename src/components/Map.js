b71f64552e5465f6a45d6a17258b5c9babc18fff
Sebastian Felix Zappe <hypo@me.com>
2017-07-03 17:03:00 +0200

    Fix a typo in Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -88,7 +88,7 @@ export default class Map extends Component {
     L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${config.accessToken}`, {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
-      id: 'accesssibility-cloud',
+      id: 'accessibility-cloud',
     }).addTo(this.map);
 
     const markerClusterGroup = new L.MarkerClusterGroup({
