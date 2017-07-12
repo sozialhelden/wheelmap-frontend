c92c8f90a2d5934e60931f7cbc517bb6e8c02a1c
Sebastian Felix Zappe <hypo@me.com>
2017-07-12 15:13:06 +0200

    Add state property to address structure (Parkopedia uses this)

--- src/lib/getAddressString.js
+++ src/lib/getAddressString.js
@@ -5,6 +5,7 @@ type Parts = {
   housenumber?: ?string,
   postcode?: ?string,
   city?: ?string,
+  state?: ?string,
   country?: ?string,
 };
 
@@ -12,6 +13,7 @@ export default function getAddressString(parts: Parts): ?string {
   return [
     [parts.street, parts.housenumber].filter(Boolean).join(' '),
     [parts.postcode, parts.city].filter(Boolean).join(' '),
+    parts.state,
     parts.country,
   ].filter(Boolean).join(', ');
 }
