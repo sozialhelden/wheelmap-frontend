527baa491b2d92432f2c3e29d706a2281948c9e7
Sebastian Felix Zappe <hypo@me.com>
2017-08-03 15:04:52 +0200

    Move tokens to .env files

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,14 +1,15 @@
+debugger
 const config = {
   locateTimeout: 60 * 60 * 1000,
   defaultStartCenter: [51.505, -0.09],
-  mapboxAccessToken: 'pk.eyJ1Ijoic296aWFsaGVsZGVuIiwiYSI6IkdUY09sSmsifQ.6vkpci46vdS7m5Jeb_YTbA',
-  wheelmapApiKey: '3s8GNQvBCmwm45zro_jP',
-  accessibilityCloudAppToken: '27be4b5216aced82122d7cf8f69e4a07',
   maxZoom: 18,
   minimalZoom: {
     withSetCategory: 13,
     withoutSetCategory: 16,
   },
+  mapboxAccessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+  wheelmapApiKey: process.env.REACT_APP_WHEELMAP_API_KEY,
+  accessibilityCloudAppToken: process.env.REACT_APP_ACCESSIBILITY_CLOUD_APP_TOKEN,
 };
 
 export default config;
