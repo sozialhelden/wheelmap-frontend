d857c6b39320298c2b91b2dd01112b871328f9d0
Sebastian Felix Zappe <hypo@me.com>
2017-08-12 04:42:07 +0200

    Remove a SVG comment from an icon

--- src/components/icons/actions/ChevronRight.js
+++ src/components/icons/actions/ChevronRight.js
@@ -3,10 +3,6 @@ import React from 'react';
 export default function ChevronRight(props) {
   return (
     <svg width="5px" height="12px" viewBox="0 0 5 12" version="1.1" {...props}>
-        {/* Generator: Sketch 44.1 (41455) - http://www.bohemiancoding.com/sketch */}
-        <title>chevron-right</title>
-      
-      
       <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
         <g id="chevron-right" fill="#000000">
           <polygon points="0 0 1 0 5 5.5 1 11.3333333 0 11.3333333 4 5.5" />
