0115f7a0a05f6d73dda12c45ae8334211927a642
Sebastian Felix Zappe <hypo@me.com>
2017-08-12 04:42:29 +0200

    Fix indentation in overrideLeafletZoomBehavior.js

--- src/components/Map/overrideLeafletZoomBehavior.js
+++ src/components/Map/overrideLeafletZoomBehavior.js
@@ -4,23 +4,23 @@ let lastScroll = new Date().getTime();
 
 export default function overrideLeafletZoomBehavior() {
   L.Map.ScrollWheelZoom.prototype._onWheelScroll = function (e) {
-      if (new Date().getTime() - lastScroll < 400) { return; }
-      var delta = L.DomEvent.getWheelDelta(e);
-      var debounce = this._map.options.wheelDebounceTime;
+    if (new Date().getTime() - lastScroll < 400) { return; }
+    const delta = L.DomEvent.getWheelDelta(e);
+    const debounce = this._map.options.wheelDebounceTime;
 
-      this._delta += delta;
-      this._lastMousePos = this._map.mouseEventToContainerPoint(e);
+    this._delta += delta;
+    this._lastMousePos = this._map.mouseEventToContainerPoint(e);
 
-      if (!this._startTime) {
-          this._startTime = +new Date();
-      }
+    if (!this._startTime) {
+        this._startTime = +new Date();
+    }
 
-      var left = Math.max(debounce - (+new Date() - this._startTime), 0);
+    const left = Math.max(debounce - (+new Date() - this._startTime), 0);
 
-      clearTimeout(this._timer);
-      lastScroll = new Date().getTime();
-      this._timer = setTimeout(L.bind(this._performZoom, this), left);
+    clearTimeout(this._timer);
+    lastScroll = new Date().getTime();
+    this._timer = setTimeout(L.bind(this._performZoom, this), left);
 
-      L.DomEvent.stop(e);
-  }
+    L.DomEvent.stop(e);
+  };
 }
