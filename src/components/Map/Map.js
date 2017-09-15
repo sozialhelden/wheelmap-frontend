7acfd3a4b1581e2103369f8fac27fefb61676d12
Sebastian Felix Zappe <hypo@me.com>
2017-09-15 16:52:35 +0200

    Remove an obsolete line of code

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -124,7 +124,6 @@ export default class Map extends Component<Props, State> {
     }).addTo(map);
 
     addLocateControlToMap(map);
-    // addFilterControlToMap(this.map);
 
     L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${config.mapboxAccessToken}`, {
       attribution: 'map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
