3c735fb739ae36449274564e2f0108e6c44bd9c3
Antonio Dakeyras <dakeyras7@posteo.de>
2017-10-18 15:57:52 +0200

    Add focus logic to NodeFooter

--- src/components/NodeToolbar/NodeFooter.js
+++ src/components/NodeToolbar/NodeFooter.js
@@ -2,6 +2,7 @@
 
 import { t } from '../../lib/i18n';
 import * as React from 'react';
+import { findDOMNode } from 'react-dom';
 import { hsl } from 'd3-color';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
@@ -90,65 +91,79 @@ type Props = {
 };
 
 
-export default function NodeFooter(props: Props) {
-  const { feature, featureId } = props;
-  const isWheelmap = isWheelmapFeatureId(featureId);
-
-  let editHint = null;
-  let needsContribution = false;
-
-  // translator: Button caption in the place toolbar
-  let editButtonCaption = t`Mark this place`;
-
-  // translator: Button caption in the place toolbar. Button navigates to Wheelmap classic.
-  const detailsButtonCaption = t`Details`;
-
-  if (feature && feature.properties) {
-    const properties = feature.properties;
-
-    // translator: Text that incentivizes the user to edit a place's accessibility.
-    const basicAccessibilityEditHint = t`Improve your karma!`;
-
-    // translator: Text that incentivizes the user to edit a place's toilet accessibility.
-    const toiletEditHint = t`Bonus karma points!`;
-
-    switch (isWheelchairAccessible(properties)) {
-      case 'unknown':
-        editHint = (<span className="edit-hint">{basicAccessibilityEditHint}</span>);
-        needsContribution = true;
-        break;
-      case 'limited':
-      case 'yes':
-        if (hasAccessibleToilet(properties) === 'unknown') {
-          editHint = (<span className="edit-hint">{toiletEditHint}</span>);
-          needsContribution = true;
-          // translator: Button caption, shown in the place toolbar
-          editButtonCaption = t`Add toilet status`;
-        }
-        break;
-      default: break;
+export default class NodeFooter extends React.Component<Props> {
+  focus() {
+    if (this.contributionLink) {
+      this.contributionLink.focus()
+    } else {
+      this.detailsLink.focus();
     }
   }
 
-  let contributionLink = null;
-  if (needsContribution && featureId) {
-    contributionLink = (<Link
-      className="link-button edit-link-button"
-      to={`/beta/nodes/${featureId}/edit`}
-    >
-      <span>{editButtonCaption}</span>{editHint || null}
-    </Link>);
-  }
+  render() {
+    const { feature, featureId } = this.props;
+    const isWheelmap = isWheelmapFeatureId(featureId);
 
-  return (
-    <StyledFooter>
-      {(isWheelmap && featureId) ?
-        <div className="wheelmap-links">
-          {contributionLink}
-          <a className="link-button" href={`https://www.wheelmap.org/de/nodes/${featureId}`}>
-            {detailsButtonCaption}
-          </a>
-        </div> : null}
-    </StyledFooter>
-  );
+    let editHint = null;
+    let needsContribution = false;
+
+    // translator: Button caption in the place toolbar
+    let editButtonCaption = t`Mark this place`;
+
+    // translator: Button caption in the place toolbar. Button navigates to Wheelmap classic.
+    const detailsButtonCaption = t`Details`;
+
+    if (feature && feature.properties) {
+      const properties = feature.properties;
+
+      // translator: Text that incentivizes the user to edit a place's accessibility.
+      const basicAccessibilityEditHint = t`Improve your karma!`;
+
+      // translator: Text that incentivizes the user to edit a place's toilet accessibility.
+      const toiletEditHint = t`Bonus karma points!`;
+
+      switch (isWheelchairAccessible(properties)) {
+        case 'unknown':
+          editHint = (<span className="edit-hint">{basicAccessibilityEditHint}</span>);
+          needsContribution = true;
+          break;
+        case 'limited':
+        case 'yes':
+          if (hasAccessibleToilet(properties) === 'unknown') {
+            editHint = (<span className="edit-hint">{toiletEditHint}</span>);
+            needsContribution = true;
+            // translator: Button caption, shown in the place toolbar
+            editButtonCaption = t`Add toilet status`;
+          }
+          break;
+        default: break;
+      }
+    }
+
+    let contributionLink = null;
+    if (needsContribution && featureId) {
+      contributionLink = (<Link
+        ref={contributionLinkInstance => this.contributionLink = findDOMNode(contributionLinkInstance)}
+        className="link-button edit-link-button"
+        to={`/beta/nodes/${featureId}/edit`}
+      >
+        <span>{editButtonCaption}</span>{editHint || null}
+      </Link>);
+    }
+
+    return (
+      <StyledFooter>
+        {(isWheelmap && featureId) ?
+          <div className="wheelmap-links">
+            {contributionLink}
+            <a
+              ref={detailsLink => this.detailsLink = detailsLink}
+              className="link-button"
+              href={`https://www.wheelmap.org/de/nodes/${featureId}`}>
+              {detailsButtonCaption}
+            </a>
+          </div> : null}
+      </StyledFooter>
+    );
+  }
 }
