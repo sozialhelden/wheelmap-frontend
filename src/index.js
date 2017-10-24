4a72ebf51e0d5affec30d802be1f56cc331937fc
Antonio Dakeyras <dakeyras7@posteo.de>
2017-10-24 13:12:04 +0200

    Add react-a11y to the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import a11y from 'react-a11y';
 import App from './App';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
@@ -9,6 +10,10 @@ import 'leaflet.locatecontrol/src/L.Control.Locate.scss';
 import 'wicg-focus-ring'
 
 import registerServiceWorker from './registerServiceWorker';
+
+if (process.env.NODE_ENV === 'development') {
+  a11y(React);
+}
 
 ReactDOM.render(
   <App />,
