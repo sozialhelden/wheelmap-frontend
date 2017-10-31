ce2c1d9488989edc623f645a7390c19f533d3bbe
Antonio Dakeyras <dakeyras7@posteo.de>
2017-10-31 14:44:13 +0100

    Use react-axe instead of react-a11y

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import a11y from 'react-a11y';
+import axe from 'react-axe';
 import App from './App';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
@@ -12,17 +12,7 @@ import 'wicg-focus-ring'
 import registerServiceWorker from './registerServiceWorker';
 
 if (process.env.NODE_ENV === 'development') {
-  const options = {
-    ReactDOM: ReactDOM,
-    filterFn: name => {
-      // ignore a11y checks on components. e.g. if the component that breaks a rule is not under our control
-      const ignoreList = ["ShareButton"];
-      const nameInIgnoreList = ignoreList.indexOf(name) !== -1
-      return !nameInIgnoreList;
-    }
-  }
-
-  a11y(React, options);
+  axe(React, ReactDOM, 1000);
 }
 
 ReactDOM.render(
