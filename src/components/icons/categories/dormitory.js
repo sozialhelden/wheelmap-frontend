14d3a68dfd92682ec4b72d9469eb750b5e5371c6
Sebastian Felix Zappe <hypo@me.com>
2018-01-29 23:35:58 +0100

    Update category icon SVGs to center them

--- src/components/icons/categories/dormitory.js
+++ src/components/icons/categories/dormitory.js
@@ -3,7 +3,7 @@ import React from "react";
 const Dormitory = props => (
   <svg viewBox="0 0 15 15" width="1em" height="1em" {...props}>
     <path
-      d="M3 3v11h5v-3h3v3h1V3H3zm4 10H4v-2h3v2zm0-3H4V8h3v2zm0-3H4V5h3v2zm4 3H8V8h3v2zm0-3H8V5h3v2z"
+      d="M3 1v11h5V9h3v3h1V1H3zm4 10H4V9h3v2zm0-3H4V6h3v2zm0-3H4V3h3v2zm4 3H8V6h3v2zm0-3H8V3h3v2z"
       fill="#000"
       fillRule="evenodd"
     />
