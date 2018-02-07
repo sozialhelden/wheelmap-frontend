628b81e3cee215a4dfd1b2f4170de8d4184bb56d
Sebastian Felix Zappe <hypo@me.com>
2018-02-07 14:12:58 +0100

    Include AC widget code directly, not as package
    This makes c-3po translation of the widget part of the react app.

--- src/components/NodeToolbar/AccessibilityDetails.js
+++ src/components/NodeToolbar/AccessibilityDetails.js
@@ -1,12 +1,105 @@
+// @flow
+
+import * as React from 'react';
 import styled from 'styled-components';
+import isPlainObject from 'lodash/isPlainObject';
+import humanizeString from 'humanize-string';
+import { t } from '../../lib/i18n';
 
-// This is a component from the accessibility.cloud JS widget which shows a detailed
-// accessibility structure of a PoI from a accessibility.cloud data source.
-//
-// https://sozialhelden.github.io/accessibility-cloud-js/
-// https://github.com/sozialhelden/accessibility-cloud-js
 
-import AccessibilityDetails from 'accessibility-cloud-widget/lib/components/AccessibilityDetails';
+function formatName(name: string, properties: {}): string {
+  const string = properties[`${name}Localized`] || humanizeString(name);
+  return string.replace(/^Rating /, '');
+}
+
+
+function formatValue(value: mixed): string {
+  if (value === true) return t`Yes`;
+  if (value === false) return t`No`;
+  return String(value);
+}
+
+
+function FormatRating({ rating }: { rating: number }) {
+  const between1and5 = Math.floor(Math.min(1, Math.max(0, rating)) * 5);
+  const stars = '★★★★★'.slice(5 - between1and5);
+  return (<span aria-label={`${between1and5} stars`}>
+    <span className="stars" aria-hidden="true">{stars}</span>
+    <span className="numeric" aria-hidden="true">{between1and5}/5</span>
+  </span>);
+}
+
+
+function DetailsArray({ className, array }: { className: ?string, array: any[] }) {
+  // eslint-disable-next-line react/no-array-index-key
+  const items = array.map((e, i) => <li key={i}><AccessibilityDetails isNested={true} details={e} /></li>);
+  return <ul className={`ac-list ${className || ''}`}>{items}</ul>;
+}
+
+
+function capitalizeFirstLetter(string): string {
+  return string.charAt(0).toLocaleUpperCase() + string.slice(1);
+}
+
+
+function DetailsObject(props: { className: ?string, object: {}, isNested?: boolean }) {
+  const { className, object, isNested } = props;
+  const properties = Object.keys(object).map((key) => {
+    if (key.match(/Localized/)) { return null; }
+    const value = object[key];
+    const name = formatName(key, object);
+
+    // Screen readers work better when the first letter is capitalized.
+    // If the attribute starts with a lowercase letter, there is no spoken pause
+    // between the previous attribute value and the attribute name.
+    const capitalizedName = capitalizeFirstLetter(name);
+
+    if (value && (value instanceof Array || isPlainObject(value))) {
+      return [
+        <dt data-key={key}>{capitalizedName}</dt>,
+        <dd><AccessibilityDetails isNested={true} details={value} /></dd>,
+      ];
+    }
+    if (key.startsWith('rating')) {
+      return [
+        <dt className="ac-rating">{capitalizedName}:</dt>,
+        <dd><FormatRating rating={parseFloat(String(value))} /></dd>,
+      ];
+    }
+    const generatedClassName = `ac-${typeof value}`;
+    const formattedValue = formatValue(value);
+    return [
+      <dt className={generatedClassName}>{capitalizedName}:</dt>,
+      <dd className={generatedClassName} aria-label={`${formattedValue}!`}>
+        <em>{formattedValue}</em>
+      </dd>,
+    ];
+  });
+  return <dl className={`ac-group ${className || ''}`} role={isNested ? null : "text"}>{properties}</dl>;
+}
+
+
+type Props = {
+  details: any,
+  locale?: ?string,
+  isNested: boolean,
+  className?: ?string,
+};
+
+
+function AccessibilityDetails(props: Props) {
+  const details = props.details;
+  if (details instanceof Array) {
+    return <DetailsArray className={props.className} array={details} />;
+  }
+  if (isPlainObject(details)) {
+    return <DetailsObject className={props.className} object={details} isNested={props.isNested}/>;
+  }
+  return <div className={props.className}>{details}</div>;
+}
+
+
+AccessibilityDetails.defaultProps = { className: null, locale: null };
 
 
 const StyledAccessibilityDetails = styled(AccessibilityDetails)`
