2ac2056dd953149783c048a619ec4091ad4dece4
Sebastian Felix Zappe <hypo@me.com>
2018-02-22 17:52:02 +0100

    Revert removing iconv

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -318,7 +318,6 @@ module.exports = {
       // Don't precache sourcemaps (they're large) and build asset manifest:
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
     }),
-    new webpack.IgnorePlugin(/iconv-lite/),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
