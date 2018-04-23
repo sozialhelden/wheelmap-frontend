8a6053bb2352335117d2db2087e0a893da5e5341
Sebastian Felix Zappe <hypo@me.com>
2018-04-23 15:21:54 +0200

    Show phone number link correctly on iOS

--- src/components/NodeToolbar/PhoneNumberLink.js
+++ src/components/NodeToolbar/PhoneNumberLink.js
@@ -4,11 +4,6 @@ import { t } from 'c-3po';
 import * as React from 'react';
 
 export default function PhoneNumberLink({ phoneNumber }: { phoneNumber: string }) {
-  if (window.navigator.userAgent.match(/iPhone/)) {
-    return (<span className="phone-number">
-      {phoneNumber}
-    </span>);
-  }
   return (<a className="phone-number link-button" href={`tel:${phoneNumber.replace(/[^\d+]/g, '')}`}>
     {t`Call ${phoneNumber}`}
   </a>);
