b6402549552b25bd90d66feeb0e6535b4f827326
Sebastian Felix Zappe <hypo@me.com>
2018-05-08 15:09:08 +0200

    Fix image orientation bugs in photos

--- src/components/NodeToolbar/Photos/convertWheelmapPhotosToLightboxPhotos.js
+++ src/components/NodeToolbar/Photos/convertWheelmapPhotosToLightboxPhotos.js
@@ -8,7 +8,7 @@ export default function convertWheelmapPhotosToLightboxPhotos(wheelmapPhotos: Wh
     src: wheelmapPhoto.images.find(i => i.type === 'thumb_iphone_retina').url,
     srcSet: wheelmapPhoto.images.filter(i => !i.type.match(/thumb/) && !i.type.match(/gallery_preview/)).map(image => `${image.url} ${image.width}w`),
     sizes: ['(min-width: 480px) 100px,33vw'],
-    width: wheelmapPhoto.images.find(i => i.type === 'original').width,
-    height: wheelmapPhoto.images.find(i => i.type === 'original').height,
+    width: wheelmapPhoto.images.find(i => i.type === 'gallery_ipad_retina').width,
+    height: wheelmapPhoto.images.find(i => i.type === 'gallery_ipad_retina').height,
   }));
 }
