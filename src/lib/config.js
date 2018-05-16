d2f0b58a26df4c1af3af83b07c9d9b1b8e07d51c
Sebastian Felix Zappe <hypo@me.com>
2018-05-16 19:14:05 +0200

    Allow to zoom to level 20 again

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,7 +8,7 @@ if (queryParams.includeSourceIds) {
 const config = {
   locateTimeout: 60 * 60 * 1000,
   defaultStartCenter: [51.505, -0.09],
-  maxZoom: 19,
+  maxZoom: 20,
   minZoomWithSetCategory: queryParams.includeSourceIds ? 8 : 13,
   minZoomWithoutSetCategory: 16,
   mapboxTileUrl: `https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`,
