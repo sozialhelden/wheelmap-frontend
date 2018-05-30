5abe480c59d1fc41b376c87c0715dbb3d3ad1b88
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-05-30 17:18:20 +0200

    Aligned locate behavior with expectations

--- src/components/Map/addLocateControlToMap.js
+++ src/components/Map/addLocateControlToMap.js
@@ -1,18 +1,27 @@
 // @flow
 
+import { t } from 'c-3po';
 import L from 'leaflet';
 import LeafletLocateControl from 'leaflet.locatecontrol/src/L.Control.Locate';
 import 'leaflet.locatecontrol';
 
+import savedState, { saveState } from '../../lib/savedState';
+
 window.L = L;
 
-
 export default function addLocateControlToMap(map: L.Map) {
-  new LeafletLocateControl({
+  const control = new LeafletLocateControl({
     position: 'topright',
     icon: 'leaflet-icon-locate',
     iconLoading: 'leaflet-icon-locate-loading',
     showPopup: false,
+    // do not follow user
+    setView: false, 
+    clickBehavior: { 
+      // only disable when pos is on screen
+      inView: 'stop', 
+      outOfView: 'setView' 
+    },
     circleStyle: {
       color: '#1fabd9',
       fillColor: '#1fabd9',
@@ -24,10 +33,25 @@ export default function addLocateControlToMap(map: L.Map) {
       fillColor: '#1fabd9',
     },
     strings: {
-      title: 'Show me where I am',
+      // translator: Locate button aria-label on the map
+      title: t`Show me where I am`,
     },
     locateOptions: {
       enableHighAccuracy: true,
+      watch: true,
+      // do not follow user 2
+      setView: false, 
     },
   }).addTo(map);
+  
+  // save state change on click
+  L.DomEvent.on(control._link, 'click', () => {
+    savedState.map.locate = control._active;
+    saveState({ 'map.locate': savedState.map.locate ? 'true' : 'false' });
+  });
+
+  // re-enable last state
+  if (savedState.map.locate) {
+    control.start();
+  }
 }
