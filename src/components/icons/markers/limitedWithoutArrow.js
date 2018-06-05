61cca34d111d09e40fbfcdf7ce2e944fb2eb609c
Sebastian Felix Zappe <hypo@me.com>
2018-06-05 15:02:00 +0200

    Update / optimize marker background SVGs

--- src/components/icons/markers/limitedWithoutArrow.js
+++ src/components/icons/markers/limitedWithoutArrow.js
@@ -3,8 +3,8 @@ import React from "react";
 const LimitedWithoutArrow = props => (
   <svg viewBox="0 0 25 25" width="1em" height="1em" {...props}>
     <path
-      d="M12.517 1l9.544 5.51v11.02l-9.544 5.51-9.543-5.51V6.51z"
       fill="#FC9B32"
+      d="M6.743 2.364h11.55l5.776 10.003-5.775 10.003H6.743L.967 12.367z"
       fillRule="evenodd"
     />
   </svg>
