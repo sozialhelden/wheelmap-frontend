1566c45a683bb2f268a8bc3e7b26f57f64d4a4cf
Sebastian Felix Zappe <hypo@me.com>
2018-06-06 23:52:34 +0200

    Always show search toolbar on desktop

--- src/components/NodeToolbar/NodeToolbar.js
+++ src/components/NodeToolbar/NodeToolbar.js
@@ -76,9 +76,14 @@ type State = {
 const StyledToolbar = styled(Toolbar)`
   hyphens: auto;
 
-  top: 50px;
+  top: 110px;
   max-height: calc(100% - 135px);
 
+  @media (max-width: 512px), (max-height: 512px) {
+    top: 50px;
+    max-height: calc(100% - 65px);
+  }
+ 
   p.sources {
     margin-top: .5em;
     font-size: 80%;
