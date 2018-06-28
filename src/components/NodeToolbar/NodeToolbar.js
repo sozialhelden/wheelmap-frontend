ce2a7e3e870d59503addebaeb2196a2563e31e7a
Felix Zappe <dinge@me.com>
2018-06-28 10:48:15 +0200

    Remove an unnecessary comment

--- src/components/NodeToolbar/NodeToolbar.js
+++ src/components/NodeToolbar/NodeToolbar.js
@@ -48,7 +48,6 @@ function filterAccessibility(properties: MinimalAccessibility): ?MinimalAccessib
 }
 
 const PositionedCloseLink = styled(CloseLink)`
-  /* top: 5px; */
   margin: -5px -16px -2px -2px; /* move close button to the same position as in search toolbar */
 `;
 PositionedCloseLink.displayName = 'PositionedCloseLink';
