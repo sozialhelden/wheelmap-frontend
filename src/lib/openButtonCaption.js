f271bfc9ffb2de4263e25e941b46d5bf2356486c
Sebastian Felix Zappe <hypo@me.com>
2018-07-16 16:45:01 +0200

    Move translator comment to `openButtonCaption.js`

--- src/lib/openButtonCaption.js
+++ src/lib/openButtonCaption.js
@@ -2,5 +2,6 @@ import { t } from 'c-3po';
 
 // The naming of `sourceNameString` is to have a consistent translation on transifex for all 'open on ...' links.
 export default function openButtonCaption(sourceNameString) {
+  // translator: Button caption shown in the place toolbar.
   return t`Open on ${ sourceNameString }`;
 }
