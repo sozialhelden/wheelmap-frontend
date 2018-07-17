4b71aab772fd0d3d804c9e3b5432f6700155e720
Sebastian Felix Zappe <hypo@me.com>
2018-07-17 19:41:47 +0200

    Add / cleanup action icons for place details panel

---
+++ src/components/icons/actions/ShareIOS.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ShareIos = props => (
+  <svg
+    viewBox="0 0 18 27"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <defs>
+      <path
+        d="M17 5.414V16h-2V5.414l-2.293 2.293-1.414-1.414L16 1.586l4.707 4.707-1.414 1.414L17 5.414zM13 10v2H9v14h14V12h-4v-2h6v18H7V10h6z"
+        id="a"
+      />
+    </defs>
+    <use
+      fill="#37404D"
+      fillRule="nonzero"
+      xlinkHref="#a"
+      transform="translate(-7 -1)"
+    />
+  </svg>
+);
+
+export default ShareIos;
+
