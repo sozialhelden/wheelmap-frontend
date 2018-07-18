2a47784dbdf4e1ebe468e3e4bf581702c515eae1
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-07-18 17:03:23 +0200

    Generate src set urls to fetch photos through ac

--- src/components/NodeToolbar/Photos/convertAcPhotosToLightboxPhotos.js
+++ src/components/NodeToolbar/Photos/convertAcPhotosToLightboxPhotos.js
@@ -1,13 +1,42 @@
 // @flow
 
+import config from '../../../lib/config';
+
 import type { PhotoModel } from './PhotoModel';
-import type { AccessibilityCloudImages } from '../../../lib/Feature';
+import type { AccessibilityCloudImage, AccessibilityCloudImages } from '../../../lib/Feature';
+
+const makeSrcUrl = (acPhoto: AccessibilityCloudImage, size: number) => {
+  return `${config.accessibilityCloudBaseUrl}/images/scale/${acPhoto.imagePath}?fitw=${size}&fith=${size}`;
+}
+
+const makeCachedImageSrcSetEntry = (acPhoto: AccessibilityCloudImage, size: number) => {
+  return `${makeSrcUrl(acPhoto, size)} ${size}w`;
+}
+
+const thumbnailSizes = [96, 192, 384];
+const thumbnailMediaSelector = [
+      `(min-resolution: 192dpi) 300px`,
+      `(min-resolution: 120dpi) 200px`,
+      `100px`];
+
+const fullScreenSizes = [480, 960, 1920];
+const fullScreenMediaSelector = [
+      `(min-width: 480) 480px`,
+      `(min-width: 960) 960px`,
+      `(min-width: 1920) 1920px`,
+      `960px`];
+
+const makeSrcSet = (sizes: number[],  acPhoto: AccessibilityCloudImage) => {
+  return sizes.map(s => makeCachedImageSrcSetEntry(acPhoto, s));
+}
 
 export default function convertAcPhotosToLightboxPhotos(acPhotos: AccessibilityCloudImages): PhotoModel[] {
   return acPhotos.images.map(acPhoto => ({
-    src: acPhoto.url,
-    srcSet: [acPhoto.url],
-    sizes: ['(min-width: 480px) 100px,33vw'],
+    src: makeSrcUrl(acPhoto, 1366),
+    srcSet: makeSrcSet(fullScreenSizes, acPhoto),
+    sizes: fullScreenMediaSelector,
+    thumbnailSrcSet: makeSrcSet(thumbnailSizes, acPhoto),
+    thumbnailSizes: thumbnailMediaSelector,
     width: acPhoto.dimensions ? acPhoto.dimensions.width : 1,
     height: acPhoto.dimensions ? acPhoto.dimensions.height : 1,
     imageId: acPhoto._id,
