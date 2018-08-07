09e5ea398d7cc03eb92aef68325a585c76d8ec1e
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-08-07 16:01:27 +0200

    Babel plugin for c-3po replacing
    Replaces the content of any t`` template string with the new value from a po file

---
+++ retranslate/babel-plugin-retranslate/src/plugin.js
@@ -0,0 +1,77 @@
+
+import * as babylon from "babylon";
+import traverse from "babel-traverse";
+import generate from "babel-generator";
+import gettextParser from "gettext-parser";
+import * as fs from "fs";
+
+
+
+var parseOptions = {
+  allowReturnOutsideFunction: true,
+  allowSuperOutsideMethod: true,
+  preserveComments: false
+};
+
+let parsedGettext = null;
+let lookupCache = {};
+
+function fetchTranslationForTemplate(fetchKey, path) {
+  if (!parsedGettext) {
+    // load po file and store in lookup
+    var content = fs.readFileSync(path);
+    parsedGettext = gettextParser.po.parse(content);
+    var translations = parsedGettext.translations[''];
+    for (const key in translations) {
+      // remove spaces around variables eg. ${ count }
+      const cleanedKey = key.replace(/(\$\{\s+)/g, "${").replace(/(\s+})/g, "}");
+      lookupCache[cleanedKey] = translations[key];
+    }
+  }
+
+  return lookupCache[fetchKey];
+}
+
+function generateCodeFromTemplateAst(ast) {
+  const generated = generate(ast);
+  const code = generated.code;
+  // remove ` ` around code 
+  return code.substr(1, code.length - 2);
+}
+
+// build an ast from a string
+function generateAstFromTemplateString(input) {
+  var parsed = babylon.parse(`\`${input}\``, parseOptions);
+  var ast = traverse.removeProperties(parsed, { preserveComments: parseOptions.preserveComments });
+  return ast.program.body[0].expression;
+}
+
+// go trough the source and replace any t`` occurrences
+const rootVisitor = {
+    visitor: {
+      TaggedTemplateExpression(path, state) {
+        const name = path.node.tag.name;
+
+        // its a TaggedTemplateExpression with the name t
+        if (name === 't') {
+          // convert current ast path into something more easily compared
+          const currentEntry = generateCodeFromTemplateAst(path.node.quasi);
+          // fetch the translation for the entry
+          const translation = fetchTranslationForTemplate(currentEntry, state.opts.replacement);
+
+          if (translation) {
+            // take the first translation and generate the ast from it
+            var newQuasis = generateAstFromTemplateString(translation.msgstr[0]);
+            // override original value
+            path.node.quasi = newQuasis;
+          } else {
+            console.warn(`Could not find translation for \`${currentEntry}\`. Ensure your po files are up-to-date and that they contain all strings.`);
+          }
+        }
+      }
+    }
+};
+
+export default function () {
+  return rootVisitor;
+}
