74b4912a5c66b6608a22f4586c0ca1214ddac65f
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-08-08 10:58:28 +0200

    Renamed TARGET_POT var to something better

--- retranslate/run.sh
+++ retranslate/run.sh
@@ -2,9 +2,9 @@
 yarn run babel \
   -f retranslate/babel-plugin-retranslate/.babelrc \
   retranslate/babel-plugin-retranslate/src/plugin.js \
-  -o retranslate/babel-plugin-retranslate/plugin.js
+  -o retranslate/babel-plugin-retranslate/dist-plugin.js
 
-export TARGET_POT=${TARGET_POT:-'./public/i18n/en_US.txt'}
+export NEW_SOURCE_PO=${NEW_SOURCE_PO:-'./public/i18n/en_US.txt'}
 
 # run plugin on test
 yarn run babel \
