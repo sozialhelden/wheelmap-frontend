40ccf48d87cd302c33eda5bbbaecf3c2ab2f6d2f
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-08-08 11:53:57 +0200

    Combine both retranslate scripts in run.sh

--- retranslate/run.sh
+++ retranslate/run.sh
@@ -1,12 +1,27 @@
+# setup environment
+export NEW_SOURCE_PO=${NEW_SOURCE_PO:-'./public/i18n/en_US.txt'}
+export NODE_ENV=development
+
+# ensure i18n folder is reset 
+git checkout -- public/i18n/ src
+
+# build rewrite script
+yarn run babel \
+  -f retranslate/rewrite-pos/.babelrc \
+  retranslate/rewrite-pos/rewrite-pos.js \
+  -o retranslate/rewrite-pos/dist-rewrite-pos.js
+
+# run script
+yarn node retranslate/rewrite-pos/dist-rewrite-pos.js
+
 # build plugin
 yarn run babel \
   -f retranslate/babel-plugin-retranslate/.babelrc \
   retranslate/babel-plugin-retranslate/src/plugin.js \
   -o retranslate/babel-plugin-retranslate/dist-plugin.js
 
-export NEW_SOURCE_PO=${NEW_SOURCE_PO:-'./public/i18n/en_US.txt'}
-
 # run plugin on test
 yarn run babel \
-  -f retranslate/.babelrc \
-  retranslate/test.js
+  -f .babelrc \
+  src \
+  --out-dir src
