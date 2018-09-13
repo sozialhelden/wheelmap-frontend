a3960574ecdb497e187f96c1ea61af2145c32c19
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-09-13 17:44:42 +0200

    Switch to next config instead of process.env

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,10 +1,14 @@
 import { getQueryParams } from './queryParams';
+import getConfig from 'next/config';
 
 let sourceIdParams = 'excludeSourceIds=LiBTS67TjmBcXdEmX';
 const queryParams = getQueryParams();
 if (queryParams.includeSourceIds) {
   sourceIdParams = `includeSourceIds=${queryParams.includeSourceIds}`;
 }
+
+const { publicRuntimeConfig } = getConfig();
+
 const config = {
   locateTimeout: 60 * 60 * 1000,
   // If no location is known, start at the Cologne cathedral landmark
@@ -13,20 +17,23 @@ const config = {
   minZoomWithSetCategory: queryParams.includeSourceIds ? 8 : 13,
   minZoomWithoutSetCategory: 16,
   mapboxTileUrl: `https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${
-    process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+    publicRuntimeConfig.REACT_APP_MAPBOX_ACCESS_TOKEN
   }`,
-  wheelmapApiKey: queryParams.includeSourceIds ? null : process.env.REACT_APP_WHEELMAP_API_KEY,
-  accessibilityCloudAppToken: process.env.REACT_APP_ACCESSIBILITY_CLOUD_APP_TOKEN,
-  accessibilityCloudBaseUrl: process.env.REACT_APP_ACCESSIBILITY_CLOUD_BASE_URL,
-  accessibilityCloudUncachedBaseUrl: process.env.REACT_APP_ACCESSIBILITY_CLOUD_UNCACHED_BASE_URL,
+  wheelmapApiKey: queryParams.includeSourceIds
+    ? null
+    : publicRuntimeConfig.REACT_APP_WHEELMAP_API_KEY,
+  accessibilityCloudAppToken: publicRuntimeConfig.REACT_APP_ACCESSIBILITY_CLOUD_APP_TOKEN,
+  accessibilityCloudBaseUrl: publicRuntimeConfig.REACT_APP_ACCESSIBILITY_CLOUD_BASE_URL,
+  accessibilityCloudUncachedBaseUrl:
+    publicRuntimeConfig.REACT_APP_ACCESSIBILITY_CLOUD_UNCACHED_BASE_URL,
   accessibilityCloudTileUrl: locale =>
     `${
-      process.env.REACT_APP_ACCESSIBILITY_CLOUD_BASE_URL
+      publicRuntimeConfig.REACT_APP_ACCESSIBILITY_CLOUD_BASE_URL
     }/place-infos.json?${sourceIdParams}&x={x}&y={y}&z={z}&appToken=${
-      process.env.REACT_APP_ACCESSIBILITY_CLOUD_APP_TOKEN
+      publicRuntimeConfig.REACT_APP_ACCESSIBILITY_CLOUD_APP_TOKEN
     }&locale=${locale}&includePlacesWithoutAccessibility=1`,
   // set this to '' for development - use relative urls, make request to server that hosts the page
-  wheelmapApiBaseUrl: process.env.REACT_APP_WHEELMAP_API_BASE_URL || '',
+  wheelmapApiBaseUrl: publicRuntimeConfig.REACT_APP_WHEELMAP_API_BASE_URL || '',
 };
 
 export default config;
