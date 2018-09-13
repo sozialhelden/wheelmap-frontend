e42730f6e324bbf3636de79cff151011cadfdac8
Sebastian Felix Zappe <hypo@me.com>
2018-09-13 15:00:55 +0200

    Add index.js implementation for next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,94 +1,39 @@
 // @flow
 
-import React from 'react';
-import Link from 'next/link';
+// babel-preset-react-app uses useBuiltIn "entry". We therefore need an entry
+// polyfill import to be replaced with polyfills we need for our targeted browsers.
+import '@babel/polyfill';
 
-import styled from 'styled-components';
+// import Link from 'next/link';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`;
+import * as React from 'react';
+// import ReactDOM from 'react-dom';
+// import axe from 'react-axe';
+import App from '../App';
+// import './index.css';
+// import 'leaflet/dist/leaflet.css';
+// import './App.css';
+// import './Map.css';
+import 'wicg-focus-ring';
 
-const Home = () => (
-  <div>
-    <div className="hero">
-      <Title className="title">Hummel to NextWheelmap!</Title>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+// import registerServiceWorker from './registerServiceWorker';
 
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+// const a11yAuditActive = false;
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+// if (process.env.NODE_ENV === 'development' && a11yAuditActive) {
+//   axe(React, ReactDOM, 1000);
+// }
 
-export default Home;
+// if (window.cordova) {
+//   document.addEventListener('deviceready', startApp, false);
+// }
+
+// Don't let the body scroll.
+// document.body.addEventListener('touchmove', e => e.preventDefault(), false);
+// document.addEventListener('touchmove', e => e.preventDefault(), false);
+
+// registerServiceWorker();
+
+export default function index() {
+  return <App />;
+}
