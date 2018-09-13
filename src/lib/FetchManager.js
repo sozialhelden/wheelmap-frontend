47097839d6898cc545ddcc38ef2765677e84de67
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-09-13 16:59:18 +0200

    Excluded map tracking from FeatureManager
    - to be reenabled later

--- src/lib/FetchManager.js
+++ src/lib/FetchManager.js
@@ -2,9 +2,9 @@
 
 import EventTarget, { CustomEvent } from './EventTarget';
 import customFetch from '../lib/fetch';
-import L from 'leaflet';
 
-type LayerEvent = Event & { layer: L.Layer, id: ?string, target: L.Layer };
+// import L from 'leaflet';
+// type LayerEvent = Event & { layer: L.Layer, id: ?string, target: L.Layer };
 
 export default class FetchManager extends EventTarget {
   lastError: ?Error = null;
@@ -21,88 +21,88 @@ export default class FetchManager extends EventTarget {
     return count > 0;
   }
 
-  registerMap(map: L.Map) {
-    // Add listeners for begin and end of load to any layers already on the map
-    map.eachLayer(function(layer) {
-      if (!layer.on) return;
-      layer.on(
-        {
-          loading: this._handleLoading,
-          load: this._handleLoad,
-        },
-        this
-      );
-    }, this);
-
-    // When a layer is added to the map, add listeners for begin and end of load
-    map.on('layeradd', this.onLayerAdded, this);
-    map.on('layerremove', this.onLayerRemoved, this);
-  }
-
-  unregisterMap(map: L.Map) {
-    // Remove listeners for begin and end of load from all layers
-    map.eachLayer(function(layer) {
-      if (!layer.off) return;
-      layer.off(
-        {
-          loading: this.onLayerLoadingStarted,
-          load: this.onLayerLoaded,
-        },
-        this
-      );
-    }, this);
-
-    // Remove layeradd/layerremove listener from map
-    map.off('layeradd', this.onLayerAdded, this);
-    map.off('layerremove', this.onLayerRemoved, this);
-
-    this.layerStatus = {};
-  }
-
-  onLayerAdded(e: LayerEvent) {
-    if (!e.layer || !e.layer.on) return;
-
-    e.layer.on(
-      {
-        loading: this.onLayerLoadingStarted,
-        load: this.onLayerLoaded,
-      },
-      this
-    );
-  }
-
-  onLayerRemoved(e: LayerEvent) {
-    if (!e.layer || !e.layer.off) return;
-
-    e.layer.off(
-      {
-        loading: this.onLayerLoadingStarted,
-        load: this.onLayerLoaded,
-      },
-      this
-    );
-  }
-
-  onLayerLoadingStarted(e: LayerEvent) {
-    const id = this.getEventTargetId(e);
-    this.layerStatus[id] = true;
-    this.dispatchEvent(new CustomEvent('start', { target: this }));
-  }
-
-  onLayerLoaded(e: LayerEvent) {
-    const id = this.getEventTargetId(e);
-    this.layerStatus[id] = false;
-    this.dispatchEvent(new CustomEvent('stop', { target: this }));
-  }
-
-  getEventTargetId(e: LayerEvent): string {
-    if (e.id) {
-      return e.id;
-    } else if (e.layer) {
-      return e.layer._leaflet_id;
-    }
-    return e.target._leaflet_id;
-  }
+  // registerMap(map: L.Map) {
+  //   // Add listeners for begin and end of load to any layers already on the map
+  //   map.eachLayer(function(layer) {
+  //     if (!layer.on) return;
+  //     layer.on(
+  //       {
+  //         loading: this._handleLoading,
+  //         load: this._handleLoad,
+  //       },
+  //       this
+  //     );
+  //   }, this);
+  //
+  //   // When a layer is added to the map, add listeners for begin and end of load
+  //   map.on('layeradd', this.onLayerAdded, this);
+  //   map.on('layerremove', this.onLayerRemoved, this);
+  // }
+  //
+  // unregisterMap(map: L.Map) {
+  //   // Remove listeners for begin and end of load from all layers
+  //   map.eachLayer(function(layer) {
+  //     if (!layer.off) return;
+  //     layer.off(
+  //       {
+  //         loading: this.onLayerLoadingStarted,
+  //         load: this.onLayerLoaded,
+  //       },
+  //       this
+  //     );
+  //   }, this);
+  //
+  //   // Remove layeradd/layerremove listener from map
+  //   map.off('layeradd', this.onLayerAdded, this);
+  //   map.off('layerremove', this.onLayerRemoved, this);
+  //
+  //   this.layerStatus = {};
+  // }
+  //
+  // onLayerAdded(e: LayerEvent) {
+  //   if (!e.layer || !e.layer.on) return;
+  //
+  //   e.layer.on(
+  //     {
+  //       loading: this.onLayerLoadingStarted,
+  //       load: this.onLayerLoaded,
+  //     },
+  //     this
+  //   );
+  // }
+  //
+  // onLayerRemoved(e: LayerEvent) {
+  //   if (!e.layer || !e.layer.off) return;
+  //
+  //   e.layer.off(
+  //     {
+  //       loading: this.onLayerLoadingStarted,
+  //       load: this.onLayerLoaded,
+  //     },
+  //     this
+  //   );
+  // }
+  //
+  // onLayerLoadingStarted(e: LayerEvent) {
+  //   const id = this.getEventTargetId(e);
+  //   this.layerStatus[id] = true;
+  //   this.dispatchEvent(new CustomEvent('start', { target: this }));
+  // }
+  //
+  // onLayerLoaded(e: LayerEvent) {
+  //   const id = this.getEventTargetId(e);
+  //   this.layerStatus[id] = false;
+  //   this.dispatchEvent(new CustomEvent('stop', { target: this }));
+  // }
+  //
+  // getEventTargetId(e: LayerEvent): string {
+  //   if (e.id) {
+  //     return e.id;
+  //   } else if (e.layer) {
+  //     return e.layer._leaflet_id;
+  //   }
+  //   return e.target._leaflet_id;
+  // }
 
   fetch(input: RequestInfo, init?: any): Promise<Response> {
     let promise = null;
