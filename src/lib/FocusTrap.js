96f0d6a4567414193214b313a6d3a956cbe5d612
Taner Aydin <taner.aydin@posteo.org>
2018-09-17 15:35:10 +0200

    Make FocusTrap component features close to focus-trap-react lib

--- src/lib/FocusTrap.js
+++ src/lib/FocusTrap.js
@@ -29,13 +29,37 @@ class FocusTrap extends React.Component<Props> {
     };
 
     const topHtmlElement = findDOMNode(this.node);
-
     this.focusTrap = createFocusTrap(topHtmlElement, tailoredFocusTrapOptions);
+
+    if (this.props.active) {
+      this.focusTrap.activate();
+    }
+    if (this.props.paused) {
+      this.focusTrap.pause();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.active && !this.props.active) {
+      this.focusTrap.deactivate();
+    } else if (!prevProps.active && this.props.active) {
+      this.focusTrap.activate();
+    }
+
+    if (prevProps.paused && !this.props.paused) {
+      this.focusTrap.unpause();
+    } else if (!prevProps.paused && this.props.paused) {
+      this.focusTrap.pause();
+    }
   }
 
   componentWillUnmount() {
     this.focusTrap.deactivate();
-    if (this.previouslyFocusedElement && this.previouslyFocusedElement.focus) {
+    if (
+      this.props.focusTrapOptions.returnFocusOnDeactivate !== false &&
+      this.previouslyFocusedElement &&
+      this.previouslyFocusedElement.focus
+    ) {
       this.previouslyFocusedElement.focus();
     }
   }
