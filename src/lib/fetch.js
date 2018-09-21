d78d52a4b9ead1ecf884f421ea0a25b8375dd837
Sebastian Felix Zappe <hypo@me.com>
2018-09-21 12:34:47 +0200

    Add a console.log to server-side fetch for debugging

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -8,6 +8,8 @@ import isCordova from './isCordova';
 export default function(url, options) {
   options = options || {};
 
+  console.log('Fetching', url, '…');
+
   // If we're running in a cordova app and it has http, make our lives easier by using native HTTP
   // connections.
   if (isCordova() && options.cordova === true) {
