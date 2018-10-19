3c9514654759d577969deb136d4c6ae77da29cc8
Lennart Hildebrandt <hello@lennerd.com>
2018-10-19 15:35:42 +0200

    Add ttl for data source and license.

--- src/lib/cache/LicenseCache.js
+++ src/lib/cache/LicenseCache.js
@@ -6,12 +6,10 @@ import env from '../env';
 export type License = { _id: string };
 
 export default class LicenseCache extends URLDataCache<License> {
-  getLicenseWithId(
-    id: string,
-    options?: { useCache: boolean } = { useCache: true }
-  ): Promise<License> {
+  getLicenseWithId(id: string): Promise<License> {
     const url = this.urlFromId(id);
-    return this.getData(url, options);
+
+    return this.getData(url);
   }
 
   urlFromId(id: string) {
@@ -19,6 +17,14 @@ export default class LicenseCache extends URLDataCache<License> {
       env.public.accessibilityCloud.appToken
     }`;
   }
+
+  injectLicense(license: License) {
+    const url = this.urlFromId(license._id);
+
+    this.inject(url, license);
+  }
 }
 
-export const licenseCache = new LicenseCache();
+export const licenseCache = new LicenseCache({
+  ttl: 1000 * 60 * 60,
+});
