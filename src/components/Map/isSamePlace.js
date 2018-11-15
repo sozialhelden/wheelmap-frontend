c009d325e23d6ecabc821c92a73c7a6ef0f7c2ea
Taner Aydin <taner.aydin@posteo.org>
2018-11-15 14:45:55 +0100

    Fix isSamePlace helper function to make sure to use strings for inlcusion test

--- src/components/Map/isSamePlace.js
+++ src/components/Map/isSamePlace.js
@@ -2,13 +2,14 @@
 
 import includes from 'lodash/includes';
 import levenshtein from 'fast-levenshtein';
+import { translatedStringFromObject } from '../../lib/i18n';
 import type { NodeProperties } from '../../lib/Feature';
 
 export default function isSamePlace(propertiesArray: NodeProperties[]) {
   const hasTwoPlaces = propertiesArray.length === 2;
   if (!hasTwoPlaces) return false;
 
-  const [name0, name1] = propertiesArray.map(p => p.name);
+  const [name0, name1] = propertiesArray.map(p => translatedStringFromObject(p.name));
   if (!name0 || !name1) return false;
 
   const levenshteinDistance = levenshtein.get(name0, name1, { useCollator: true });
