8a15951dcdddbebfe6772d770d2adb4233dbe287
Taner Aydin <taner.aydin@posteo.org>
2018-11-26 16:26:16 +0100

    Make all links in report dialogs pointing to OSM open in new tab

--- src/components/NodeToolbar/Report/FixPlacePosition.js
+++ src/components/NodeToolbar/Report/FixPlacePosition.js
@@ -67,6 +67,8 @@ export default class ReportProblemButton extends React.Component<Props> {
           className="link-button"
           ref={editLink => (this.editLink = editLink)}
           onKeyDown={this.trapFocus}
+          target="_blank"
+          rel="noopener noreferrer"
         >
           {editButtonCaption}
         </a>
@@ -75,6 +77,8 @@ export default class ReportProblemButton extends React.Component<Props> {
           className="link-button"
           ref={noteLink => (this.noteLink = noteLink)}
           onKeyDown={this.trapFocus}
+          target="_blank"
+          rel="noopener noreferrer"
         >
           {noteButtonCaption}
         </a>
