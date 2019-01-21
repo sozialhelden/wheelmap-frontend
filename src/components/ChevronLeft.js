73ab84fd243e2680fd5dabb64b47027293a03184
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-01-21 12:30:31 +0100

    Fixed component naming

--- src/components/ChevronLeft.js
+++ src/components/ChevronLeft.js
@@ -7,8 +7,8 @@ type Props = {
   className: string,
 };
 
-const ChevronRight = styled((props: Props) => (
-  <svg className={`chevron-right ${props.className}`} viewBox="0 0 5 12" version="1.1">
+const ChevronLeft = styled((props: Props) => (
+  <svg className={`chevron-left ${props.className}`} viewBox="0 0 5 12" version="1.1">
     <g transform="scale(-1, 1)" transform-origin="2.5 0">
       <polygon points="0 0 1 0 5 5.5 1 11.3333333 0 11.3333333 4 5.5" />
     </g>
@@ -26,4 +26,4 @@ const ChevronRight = styled((props: Props) => (
   }
 `;
 
-export default ChevronRight;
+export default ChevronLeft;
