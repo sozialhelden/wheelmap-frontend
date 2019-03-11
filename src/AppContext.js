6ce479bed28958e0315b22a07f5b68a286c92002
Taner Aydin <taner.aydin@posteo.org>
2019-03-11 18:17:28 +0100

    Remove not working typing to make compilation work

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,9 +1,16 @@
 import { createContext } from 'react';
 
+// Only extend this with value that are sensible global values that potentially might be needed
+// everywhere
 export type AppContext = {
   baseUrl: string,
 };
 
-const { Provider, Consumer } = createContext<AppContext>();
+// FIXME
+// The next line should be rather:
+// const { Provider, Consumer } = createContext<AppContext>();
+//
+// Adding the correct generic typing here produces errors during compilation though.
+const { Provider, Consumer } = createContext();
 
 export { Provider as AppContextProvider, Consumer as AppContextConsumer };
