db9a7b3622eae3c46261145898451cfeba25902b
Taner Aydin <taner.aydin@posteo.org>
2019-04-08 14:09:35 +0200

    Fix ChevronLeft transform so it works cross-browser

--- src/components/ChevronLeft.js
+++ src/components/ChevronLeft.js
@@ -9,7 +9,7 @@ type Props = {
 
 const ChevronLeft = styled((props: Props) => (
   <svg className={`chevron-left ${props.className}`} viewBox="0 0 5 12" version="1.1">
-    <g transform="scale(-1, 1)" transform-origin="2.5 0">
+    <g transform="scale(-1, 1) translate(-5, 0)">
       <polygon points="0 0 1 0 5 5.5 1 11.3333333 0 11.3333333 4 5.5" />
     </g>
   </svg>
