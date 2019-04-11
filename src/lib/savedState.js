258809dac17f401cab53a6317d1b4ff781877c6a
Taner Aydin <taner.aydin@posteo.org>
2019-04-11 17:34:29 +0200

    Use optional types where necessary in active event storage helper functions

--- src/lib/savedState.js
+++ src/lib/savedState.js
@@ -53,11 +53,11 @@ export function setAnalyticsAllowed(value: boolean) {
   return storage.setItem('wheelmap.analyticsAllowed', value ? 'true' : 'false');
 }
 
-export function getActiveMappingEventId(): string {
+export function getActiveMappingEventId(): ?string {
   return storage.getItem('wheelmap.activeMappingEventId');
 }
 
-export function setActiveMappingEventId(mappingEventId: string) {
+export function setActiveMappingEventId(mappingEventId: ?string) {
   storage.setItem('wheelmap.activeMappingEventId', mappingEventId);
 }
 
