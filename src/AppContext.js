f7bb920407a9805e875786b65c837f4d7f5c23d2
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-04-12 10:50:30 +0200

    Typed app context & added categories lookup

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,16 +1,23 @@
-import { createContext } from 'react';
+// @flow
+
+import { createContext, type Context } from 'react';
+import { type CategoryLookupTables } from './lib/Categories';
 
 // Only extend this with value that are sensible global values that potentially might be needed
 // everywhere
 export type AppContext = {
   baseUrl: string,
+  categories: CategoryLookupTables,
 };
 
-// FIXME
-// The next line should be rather:
-// const { Provider, Consumer } = createContext<AppContext>();
-//
-// Adding the correct generic typing here produces errors during compilation though.
-const { Provider, Consumer } = createContext();
+const { Provider, Consumer } = createContext<AppContext>({
+  baseUrl: '',
+  categories: {
+    synonymCache: {},
+    idsToWheelmapCategories: {},
+    wheelmapCategoryNamesToCategories: {},
+    wheelmapRootCategoryNamesToCategories: {},
+  },
+});
 
 export { Provider as AppContextProvider, Consumer as AppContextConsumer };
