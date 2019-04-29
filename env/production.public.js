db9d24e630d516a2b3db8b47333db8eef9db2ce4
Sebastian Felix Zappe <hypo@me.com>
2019-04-29 14:29:10 +0200

    Replace IP-based backend hostname with `classic.wheelmap.org`

--- env/production.public.js
+++ env/production.public.js
@@ -8,7 +8,7 @@ const config = {
   },
   wheelmap: {
     apiKey: '3s8GNQvBCmwm45zro_jP',
-    baseUrl: 'http://176.9.63.170',
+    baseUrl: 'http://classic.wheelmap.org',
   },
   accessibilityCloud: {
     appToken: '27be4b5216aced82122d7cf8f69e4a07',
