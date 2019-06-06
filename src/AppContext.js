5ec4213d0e7745766f8bcf5556e30c9471848b91
Taner Aydin <taner.aydin@posteo.org>
2019-06-06 17:06:57 +0200

    Make app in AppContent non-optional

--- src/AppContext.js
+++ src/AppContext.js
@@ -7,13 +7,20 @@ import { type App } from './lib/App';
 // Only extend this with values that are sensible global values that potentially might be needed
 // everywhere
 export type AppContext = {
-  app?: App,
+  app: App,
   baseUrl: string,
   categories: CategoryLookupTables,
   preferredLanguage: string,
 };
 
 const { Provider, Consumer } = createContext<AppContext>({
+  app: {
+    _id: '',
+    organizationId: '',
+    name: '',
+    clientSideConfiguration: {},
+    tokenString: '',
+  },
   baseUrl: '',
   categories: {
     synonymCache: {},
