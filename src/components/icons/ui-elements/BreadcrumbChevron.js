690349610f771b43b6096ca7161f77f881b47506
Taner Aydin <taner.aydin@posteo.org>
2019-06-17 13:30:07 +0200

    Recreate ui-elements svgs

--- src/components/icons/ui-elements/BreadcrumbChevron.js
+++ src/components/icons/ui-elements/BreadcrumbChevron.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const BreadcrumbChevron = props => (
-  <svg viewBox="0 0 22 50" width="1em" height="1em" {...props}>
+const SvgBreadcrumbChevron = props => (
+  <svg width="1em" height="1em" viewBox="0 0 22 50" {...props}>
     <defs>
-      <linearGradient x1="30.93%" y1="69.921%" x2="187.835%" y2="68.047%" id="a">
+      <linearGradient
+        x1="30.93%"
+        y1="69.921%"
+        x2="187.835%"
+        y2="68.047%"
+        id="BreadcrumbChevron_svg__a"
+      >
         <stop stopColor="#D0D0D0" stopOpacity={0} offset="0%" />
         <stop stopColor="#3B3B3B" stopOpacity={0.716} offset="71.581%" />
         <stop offset="100%" />
@@ -12,7 +18,7 @@ const BreadcrumbChevron = props => (
     <g fill="none" fillRule="evenodd">
       <path
         fillOpacity={0.5}
-        fill="url(#a)"
+        fill="url(#BreadcrumbChevron_svg__a)"
         d="M17.143 50H0V0h17.143l12.571 25z"
         transform="translate(-9)"
       />
@@ -21,4 +27,4 @@ const BreadcrumbChevron = props => (
   </svg>
 );
 
-export default BreadcrumbChevron;
+export default SvgBreadcrumbChevron;
