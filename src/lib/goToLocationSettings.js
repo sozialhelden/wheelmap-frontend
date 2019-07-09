e937a97610c93e4edd09f35758ae5e7ade8ef0d2
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-07-09 10:35:07 +0200

    Align location settings across platforms

--- src/lib/goToLocationSettings.js
+++ src/lib/goToLocationSettings.js
@@ -9,19 +9,24 @@ export default function goToLocationSettings() {
 
   const userAgent = getUserAgent();
 
-  const identity = userAgent.browser.name;
+  let identity = userAgent.browser.name;
+  if (userAgent.os.name === 'iOS') {
+    identity = 'iOS';
+  } else if (userAgent.os.name === 'Android') {
+    identity = 'Android';
+  }
 
   const supportURLs = {
-    // android https://support.google.com/nexus/answer/3467281
     Safari: 'https://support.apple.com/en-us/ht204690',
     'Mobile Safari': 'https://support.apple.com/en-us/ht203033',
+    iOS: 'https://support.apple.com/en-us/ht203033',
+    Android: 'https://support.google.com/android/answer/6179507',
     Chrome: 'https://support.google.com/chrome/answer/142065',
     Firefox: 'https://support.mozilla.org/en-US/kb/does-firefox-share-my-location-websites',
     Edge: 'http://www.monitorconnect.com/allow-location-tracking-on-microsoft-edge-web-solution-b/',
   };
 
   const supportURL = supportURLs[identity];
-
   if (supportURL) {
     window.open(supportURL, '_blank');
     return;
