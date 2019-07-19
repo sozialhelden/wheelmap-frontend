1161a779fb4b22e830ba57f6e109549b47f944f1
Sebastian Felix Zappe <hypo@me.com>
2019-07-20 01:59:58 +0200

    Fix next docker build / runtime starting

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
 const webpack = require('webpack');
-
-module.exports = withCss(
-  withSass({
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty',
-        dgram: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty',
-        async_hooks: 'mock',
-        'elastic-apm-node': 'empty',
-      };
-      return config;
-    },
-    // Disabling file-system routing to always use custom server.
-    useFileSystemPublicRoutes: false,
-  })
+const withBabelMinify = require('next-babel-minify')()
+module.exports = withBabelMinify(
+  withCss(
+    withSass({
+      webpack: config => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+          fs: 'empty',
+          dgram: 'empty',
+          net: 'empty',
+          tls: 'empty',
+          child_process: 'empty',
+          async_hooks: 'mock',
+          'elastic-apm-node': 'empty',
+        };
+        return config;
+      },
+      // Disabling file-system routing to always use custom server.
+      useFileSystemPublicRoutes: false,
+    })
+  )
 );
