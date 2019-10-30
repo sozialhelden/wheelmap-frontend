fc461a520a318ed4541381f58977796e120595cd
Sebastian Felix Zappe <hypo@me.com>
2019-10-30 18:51:49 +0100

    Update Next.js, allow node_modules transpilation

--- next.config.js
+++ next.config.js
@@ -1,40 +1,44 @@
 const ElasticAPMSourceMapPlugin = require('@hypo808/elastic-apm-sourcemap-webpack-plugin').default;
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
+const withTM = require('next-transpile-modules');
 const webpack = require('webpack');
 const env = require('./src/lib/env');
 const withSourceMaps = require('@zeit/next-source-maps');
 
 module.exports = withSourceMaps(
   withCss(
-    withSass({
-      webpack: config => {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-          fs: 'empty',
-          dgram: 'empty',
-          net: 'empty',
-          tls: 'empty',
-          child_process: 'empty',
-          async_hooks: 'mock',
-          'elastic-apm-node': 'empty',
-        };
+    withSass(
+      withTM({
+        transpileModules: [],
+        webpack: config => {
+          // Fixes npm packages that depend on `fs` module
+          config.node = {
+            fs: 'empty',
+            dgram: 'empty',
+            net: 'empty',
+            tls: 'empty',
+            child_process: 'empty',
+            async_hooks: 'mock',
+            'elastic-apm-node': 'empty',
+          };
 
-        // if (process.env.NODE_ENV === 'production') {
-        //   config.plugins.unshift(new ElasticAPMSourceMapPlugin({
-        //     serviceName: 'wheelmap-react-frontend',
-        //     serviceVersion: env.npm_package_version,
-        //     serverURL: env.REACT_APP_ELASTIC_APM_SERVER_URL,
-        //     publicPath: `${env.PUBLIC_URL}/_next/static/chunks`,
-        //     secret: env.ELASTIC_APM_SECRET_TOKEN,
-        //     logLevel: 'debug'
-        //   }));
-        // }
+          // if (process.env.NODE_ENV === 'production') {
+          //   config.plugins.unshift(new ElasticAPMSourceMapPlugin({
+          //     serviceName: 'wheelmap-react-frontend',
+          //     serviceVersion: env.npm_package_version,
+          //     serverURL: env.REACT_APP_ELASTIC_APM_SERVER_URL,
+          //     publicPath: `${env.PUBLIC_URL}/_next/static/chunks`,
+          //     secret: env.ELASTIC_APM_SECRET_TOKEN,
+          //     logLevel: 'debug'
+          //   }));
+          // }
 
-        return config;
-      },
-      // Disabling file-system routing to always use custom server.
-      useFileSystemPublicRoutes: false,
-    })
+          return config;
+        },
+        // Disabling file-system routing to always use custom server.
+        useFileSystemPublicRoutes: false,
+      })
+    )
   )
 );
