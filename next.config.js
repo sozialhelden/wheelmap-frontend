03b1f2f393cdf8b25850ee9fb617d4d0c41b6b99
Sebastian Felix Zappe <hypo@me.com>
2019-10-31 14:56:46 +0100

    Re-enable correct transpilation to fix IE11 support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const ElasticAPMSourceMapPlugin = require('@hypo808/elastic-apm-sourcemap-webpack-plugin').default;
+const TTagPlugin = require('babel-plugin-ttag').default;
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
-const withTM = require('next-transpile-modules');
+const withTranspileModules = require('next-transpile-modules');
 const webpack = require('webpack');
 const env = require('./src/lib/env');
 const withSourceMaps = require('@zeit/next-source-maps');
@@ -9,8 +10,15 @@ const withSourceMaps = require('@zeit/next-source-maps');
 module.exports = withSourceMaps(
   withCss(
     withSass(
-      withTM({
-        transpileModules: [],
+      withTranspileModules({
+        // Next.js doesn't transpile node_modules content by default.
+        // We have to do this manually to make IE 11 users happy.
+        transpileModules: [
+          '@sozialhelden/twelve-factor-dotenv',
+          '@elastic/apm-rum-core',
+          '@elastic/apm-rum',
+          'dotenv',
+        ],
         webpack: config => {
           // Fixes npm packages that depend on `fs` module
           config.node = {
