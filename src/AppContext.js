d3fb49d0b25f4b49ad5e6d75f73981e7daac093d
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2020-03-23 17:39:19 +0100

    Extend app context with ac-category tree

--- src/AppContext.js
+++ src/AppContext.js
@@ -6,14 +6,14 @@ import { type App } from './lib/App';
 
 // Only extend this with values that are sensible global values that potentially might be needed
 // everywhere
-export type AppContext = {
+export type AppContextData = {
   app: App,
   baseUrl: string,
   categories: CategoryLookupTables,
   preferredLanguage: string,
 };
 
-const { Provider, Consumer } = createContext<AppContext>({
+const AppContext = createContext<AppContextData>({
   app: {
     _id: '',
     organizationId: '',
@@ -27,8 +27,13 @@ const { Provider, Consumer } = createContext<AppContext>({
     idsToWheelmapCategories: {},
     wheelmapCategoryNamesToCategories: {},
     wheelmapRootCategoryNamesToCategories: {},
+    categoryTree: [],
   },
   preferredLanguage: '',
 });
 
+const { Provider, Consumer } = AppContext;
+
 export { Provider as AppContextProvider, Consumer as AppContextConsumer };
+
+export default AppContext;
