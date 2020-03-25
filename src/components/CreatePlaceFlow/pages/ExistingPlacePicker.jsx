89942ea0e6b87cf2b61203da7c9ad31d32a559d3
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2020-03-25 13:13:36 +0100

    Style existing place picker

--- src/components/CreatePlaceFlow/pages/ExistingPlacePicker.jsx
+++ src/components/CreatePlaceFlow/pages/ExistingPlacePicker.jsx
@@ -1,11 +1,19 @@
 // @flow
 import * as React from 'react';
-import VerticalPage from '../components/VerticalPage';
 import styled from 'styled-components';
+import { t } from 'ttag';
+import { Dots } from 'react-activity';
 
+import SearchResult from '../../SearchToolbar/SearchResult.js';
+
+import VerticalPage from '../components/VerticalPage';
 import usePlaceSearchWithWheelmapResolution, {
   type WheelmapResolvedSearchResultFeature,
 } from '../components/usePlaceSearchWithWheelmapResolution';
+import { ChromelessButton } from '../../Button';
+import colors from '../../../lib/colors';
+import InputField from '../components/InputField';
+import AppContext from '../../../AppContext.js';
 
 type Props = {
   className?: string,
@@ -17,6 +25,8 @@ type Props = {
 
 const ExistingPlacePicker = (props: Props) => {
   const { className, visible, onCancel, onCreateNew, onSelectExisting } = props;
+
+  const appContext = React.useContext(AppContext);
 
   const [
     searchState,
@@ -46,29 +56,55 @@ const ExistingPlacePicker = (props: Props) => {
   return (
     <VerticalPage className={className}>
       <header>
-        <button onClick={onCancelled}>Cancel</button>
-        <h2>Create Place</h2>
+        <ChromelessButton onClick={onCancelled}>{t`Cancel`}</ChromelessButton>
+        <h2>{t`Create Place`}</h2>
       </header>
 
-      <input value={searchString} onChange={onSearchStringChanged} type="text" />
-      <span>Check if the place already exists by searching for it</span>
-      <button onClick={onCreateClicked} disabled={!canCreateNewPlace}>
-        Create a new place with this name
-      </button>
-      {searchState === 'Error' && <span>An error occurred while searching</span>}
-      {searchState === 'TypeMore' && <span>Type more to start searching</span>}
-      {searchState === 'Loading' && <span>Searching...</span>}
-      {searchResults && (
-        <ul>
-          {searchResults.map((result, index) => {
-            return (
-              <li key={index}>
-                <button onClick={() => onSelectExisting(result)}>{JSON.stringify(result)}</button>
-              </li>
-            );
-          })}
-        </ul>
+      <InputField value={searchString} onChange={onSearchStringChanged} type="text" />
+      {!canCreateNewPlace && (
+        <div className="action-hint">{t`Check if the place already exists by searching for it`}</div>
       )}
+      {canCreateNewPlace && (
+        <>
+          <div className="action-hint">{t`No matching result?`}</div>
+          <ChromelessButton onClick={onCreateClicked}>
+            {t`Create a new place with this name`}
+          </ChromelessButton>
+        </>
+      )}
+
+      <div className="search-block">
+        {searchState === 'Error' && (
+          <div className="search-state error">{t`An error occurred while searching`}</div>
+        )}
+        {searchState === 'TypeMore' && (
+          <div className="search-state type-more">{t`Type more to start searching`}</div>
+        )}
+        {searchState === 'Loading' && (
+          <div className="search-state loading">
+            <div>
+              <Dots size={30} color={'rgba(0, 0, 0, 0.4)'} />
+            </div>
+            {t`Searching...`}
+          </div>
+        )}
+        {searchResults && (
+          <ul>
+            {searchResults.map((result, index) => {
+              return (
+                <SearchResult
+                  key={index}
+                  feature={result.searchResult}
+                  wheelmapFeature={result.wheelmapFeature}
+                  categories={appContext.categories}
+                  hidden={false}
+                  onClick={() => {}}
+                />
+              );
+            })}
+          </ul>
+        )}
+      </div>
     </VerticalPage>
   );
 };
@@ -77,5 +113,56 @@ export default styled(ExistingPlacePicker)`
   > header {
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px;
+
+    h2 {
+      margin: 0;
+      flex: 1;
+      text-align: center;
+    }
+  }
+
+  > .action-hint {
+    color: ${colors.textMuted};
+    text-align: center;
+  }
+
+  > .search-block {
+    margin-top: 24px;
+    border-top: 1px solid ${colors.textMuted};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    > .search-state {
+      color: ${colors.textMuted};
+      text-align: center;
+      padding: 48px;
+      font-weight: bold;
+
+      > div {
+        margin: 12px;
+      }
+    }
+
+    ul {
+      align-self: flex-start;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+
+      > li {
+        width: 100%;
+      }
+    }
+  }
+
+  ${ChromelessButton} {
+    font-weight: bold;
+    color: ${colors.linkColor};
   }
 `;
