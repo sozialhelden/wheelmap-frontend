5f547c69650f0ebe5614cf6957bee1c532af0d65
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2020-03-27 11:38:45 +0100

    Render all places

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -951,15 +951,9 @@ export default class Map extends React.Component<Props, State> {
 
   createMarkerFromFeature = (feature: Feature, latlng: [number, number]) => {
     const properties = feature && feature.properties;
-    if (!properties) return null;
-    if (
-      !isWheelmapFeature(feature) &&
-      !properties.accessibility &&
-      !includes(EquipmentCategoryStrings, properties.category)
-    ) {
+    if (!properties) {
       return null;
     }
-
     const featureId: string = properties.id || properties._id || feature._id;
 
     return new HighlightableMarker(latlng, A11yMarkerIcon, {
