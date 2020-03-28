55f9ee246ffb0a0c5f31fbd60bae88680f45bf6c
Taner Aydın <saryekad@posteo.de>
2020-03-28 13:35:30 +0100

    Add Back Button to DetailPanel

--- src/components/NodeToolbar/DetailPanelHeader.js
+++ src/components/NodeToolbar/DetailPanelHeader.js
@@ -1,7 +1,11 @@
 // @flow
 import React, { type ReactElement } from 'react';
 import styled from 'styled-components';
+import { t } from 'ttag';
+
 import colors from '../../lib/colors';
+import ChevronLeft from '../ChevronLeft';
+import { ChromelessButton } from '../Button';
 
 type Props = {
   title: string,
@@ -11,27 +15,37 @@ type Props = {
 };
 
 const DetailPanelHeader = ({ title, subtitle, icon, className }: Props) => {
+  const buttonText = t`back to map`;
   return (
     <header className={className}>
-      {icon}
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <ChromelessButton>
+        <ChevronLeft />
+        {buttonText}
+      </ChromelessButton>
+      <div>
+        {icon}
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
     </header>
   );
 };
 
 export default styled(DetailPanelHeader)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 300px;
-  margin-top: -30px;
-  margin-right: auto;
-  margin-bottom: 0;
-  margin-left: auto;
-  color: ${colors.textColor};
-  word-break: break-word;
+  position: relative;
 
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 300px;
+    margin-top: -30px;
+    margin-right: auto;
+    margin-bottom: 0;
+    margin-left: auto;
+    color: ${colors.textColor};
+    word-break: break-word;
+  }
   h1 {
     font-size: 1.5rem;
     font-weight: 400;
@@ -42,5 +56,18 @@ export default styled(DetailPanelHeader)`
   p {
     color: ${colors.textColorBrighter};
     font-size: 1rem;
+  }
+
+  ${ChromelessButton} {
+    display: flex;
+    position: absolute;
+    left: 0;
+    top: 50px;
+
+    ${ChevronLeft} {
+      margin: 0 0.5rem 0 0;
+      width: 1rem;
+      height: 2rem;
+    }
   }
 `;
