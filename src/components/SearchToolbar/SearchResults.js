ee14808ce4d64046cdec814a61f4f668e533afb1
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2020-07-21 14:59:40 +0200

    Make SearchResults compatible to React `ref` property

--- src/components/SearchToolbar/SearchResults.js
+++ src/components/SearchToolbar/SearchResults.js
@@ -20,53 +20,7 @@ type Props = {
   refFirst: ?(result: ?SearchResult) => void,
 };
 
-function SearchResults(props: Props) {
-  const id = result => result && result.properties && result.properties.osm_id;
-  const { wheelmapFeatures, features } = props.searchResults;
-
-  const failedLoading = !!props.searchResults.error;
-  const hasNoResults = !failedLoading && features.length === 0;
-
-  // translator: Text in search results when nothing was found
-  const noResultsFoundCaption = t`No results found`;
-
-  // translator: Text in search results when an error occurred
-  const searchErrorCaption = t`No results available. Please try again later!`;
-
-  const renderedFeatureIds = [];
-
-  return (
-    <ul className={`search-results ${props.className || ''}`} aria-label={t`Search results`}>
-      {failedLoading && <li className="error-result">{searchErrorCaption}</li>}
-      {hasNoResults && <li className="no-result">{noResultsFoundCaption}</li>}
-      {features.map((feature, index) => {
-        const featureId = id(feature);
-
-        if (renderedFeatureIds.indexOf(featureId) > -1) {
-          return null;
-        }
-
-        renderedFeatureIds.push(featureId);
-
-        return (
-          <SearchResult
-            feature={feature}
-            wheelmapFeature={wheelmapFeatures && wheelmapFeatures[index]}
-            key={featureId}
-            onClick={props.onSearchResultClick}
-            hidden={!!props.hidden}
-            categories={props.categories}
-            ref={ref => {
-              if (props.refFirst && index === 0) props.refFirst(ref);
-            }}
-          />
-        );
-      })}
-    </ul>
-  );
-}
-
-const StyledSearchResults = styled(SearchResults)`
+const StyledSearchResultList = styled.ul`
   list-style-type: none;
   margin: 0;
 
@@ -124,4 +78,51 @@ const StyledSearchResults = styled(SearchResults)`
   }
 `;
 
-export default StyledSearchResults;
+export default function SearchResults(props: Props) {
+  const id = result => result && result.properties && result.properties.osm_id;
+  const { wheelmapFeatures, features } = props.searchResults;
+
+  const failedLoading = !!props.searchResults.error;
+  const hasNoResults = !failedLoading && features.length === 0;
+
+  // translator: Text in search results when nothing was found
+  const noResultsFoundCaption = t`No results found`;
+
+  // translator: Text in search results when an error occurred
+  const searchErrorCaption = t`No results available. Please try again later!`;
+
+  const renderedFeatureIds = [];
+
+  return (
+    <StyledSearchResultList
+      className={`search-results ${props.className || ''}`}
+      aria-label={t`Search results`}
+    >
+      {failedLoading && <li className="error-result">{searchErrorCaption}</li>}
+      {hasNoResults && <li className="no-result">{noResultsFoundCaption}</li>}
+      {features.map((feature, index) => {
+        const featureId = id(feature);
+
+        if (renderedFeatureIds.indexOf(featureId) > -1) {
+          return null;
+        }
+
+        renderedFeatureIds.push(featureId);
+
+        return (
+          <SearchResult
+            feature={feature}
+            wheelmapFeature={wheelmapFeatures && wheelmapFeatures[index]}
+            key={featureId}
+            onClick={props.onSearchResultClick}
+            hidden={!!props.hidden}
+            categories={props.categories}
+            ref={ref => {
+              if (props.refFirst && index === 0) props.refFirst(ref);
+            }}
+          />
+        );
+      })}
+    </StyledSearchResultList>
+  );
+}
