e60accdc377db8c0afcbd22c27db6f679f771ec6
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2020-12-03 19:58:24 +0100

    Add missing liveness check HTTP endpoint

--- src/server/healthChecks.ts
+++ src/server/healthChecks.ts
@@ -2,7 +2,9 @@ export default function registerHealthChecks(app) {
   app.get('/healthy', (req, res) => {
     res.status(200).send('OK!');
   });
-
+  app.get('/healthz', (req, res) => {
+    res.status(200).send('OK!');
+  });
   app.get('/ready', (req, res) => {
     // TODO: Add logic for overload here
     res.status(200).send('OK!');
