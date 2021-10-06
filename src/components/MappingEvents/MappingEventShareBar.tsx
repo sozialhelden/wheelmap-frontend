b7488d7f6d26ee4e0a75f7750eccdfdafa773f8a
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-10-06 16:19:47 +0200

    Sync translations with transifex

--- src/components/MappingEvents/MappingEventShareBar.tsx
+++ src/components/MappingEvents/MappingEventShareBar.tsx
@@ -30,35 +30,8 @@ const MappingEventShareBar = ({
   const description = mappingEvent.description || productNameLocalized;
 
   const mailSubject = sharedObjectTitle;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
   // translator: Email text used when sharing a mapping event via email.
-<<<<<<< HEAD
-<<<<<<< HEAD
-  let mailBody = t`Help us out and join the ${productNameLocalized} mapping event ${eventName}. You can find more info here: ${url}`;
-=======
-  // translator: Email text used when sharing a mapping event via email.
-  let mailBody = t`Hi,\n\nHelp us out and join the ‘${eventName}’ mapping event!\n\nFind more info here: ${url}`;
->>>>>>> 21ade92a (Improve mapping event share text)
-=======
-
-  // translator: Email text used when sharing a mapping event via email.
-<<<<<<< HEAD
   let mailBody = t`Hi 👋\n\nHelp us out and join the mapping event ‘${eventName}’ on ${productNameLocalized}. You can find more info here:\n\n${url}\n\nSee you there!`;
->>>>>>> 4d628543 (Sync with transifex)
-=======
-  let mailBody = t`Hi 👋\n\nHelp us out and join the mapping event ‘${eventName}’ on ${productNameLocalized}.You can find more info here:\n\n${url}\n\nSee you there!`;
->>>>>>> 1ae5835f (Sync translations to JS)
-=======
-  let mailBody = t`Hi 👋\n\nHelp us out and join the mapping event ‘${eventName}’ on ${productNameLocalized}. You can find more info here:\n\n${url}\n\nSee you there!`;
->>>>>>> 410d33dc (Improve mapping event share text)
-=======
-  let mailBody = t`Hi 👋\n\nHelp us out and join the mapping event ‘${eventName}’ on ${productNameLocalized}. You can find more info here:\n\n${url}\n\nSee you there!`;
-=======
-  let mailBody = t`Help us out and join the ${productNameLocalized} mapping event ${eventName}. You can find more info here: ${url}`;
->>>>>>> a1e39f19 (Sync with transifex)
->>>>>>> e5d2a64d (Sync with transifex)
 
   const mailToLink = `mailto:?subject=${encodeURIComponent(mailSubject)}&body=${encodeURIComponent(
     mailBody
