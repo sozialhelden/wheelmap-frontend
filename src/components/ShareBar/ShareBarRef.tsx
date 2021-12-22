1290e015d168d5c836e167a8f4b8ddc82aab01b1
Julian Scheele <unknown>
2021-12-22 12:01:07 +0100

    fixed dom nesting

--- src/components/ShareBar/ShareBarRef.tsx
+++ src/components/ShareBar/ShareBarRef.tsx
@@ -1,10 +1,9 @@
 import { FunctionComponent, useRef, useState } from 'react';
-import { ChromelessButton } from '../Button';
-import { t } from 'ttag';
-import ShareIcon from '../icons/actions/ShareIOS';
-import ChevronLeft from '../ChevronLeft';
 import ShareBarToggle from './ShareBarToggle';
 import ShareBarContent from './ShareBarContent';
+import { Caption } from '../IconButton';
+import colors from '../../lib/colors';
+import styled from 'styled-components';
 
 type ShareBarProps = {
   shareButtonCaption: string;
@@ -40,15 +39,12 @@ const ShareBar: FunctionComponent<ShareBarProps> = ({
 
   if (!isExpanded) {
     return (
-      <>
-        new
-        <ShareBarToggle
-          ref={shareBarToggleRef}
-          isExpanded={isExpanded}
-          onClick={toggleShareBar}
-          caption={shareButtonCaption}
-        />
-      </>
+      <ShareBarToggle
+        ref={shareBarToggleRef}
+        isExpanded={isExpanded}
+        onClick={toggleShareBar}
+        caption={shareButtonCaption}
+      />
     );
   }
 
@@ -66,4 +62,71 @@ const ShareBar: FunctionComponent<ShareBarProps> = ({
   );
 };
 
-export default ShareBar;
+const StyledShareBar = styled(ShareBar)`
+  padding: 0 5px 0 0;
+  display: flex;
+  flex-direction: row !important;
+
+  header {
+    min-height: 38px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  button.collapse-button {
+    width: 25px;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+
+  footer {
+    flex: 1;
+    overflow: hidden;
+    max-height: 0;
+    opacity: 0;
+    transition: opacity 1s ease-out, max-height 0.15s ease-out;
+
+    &.is-visible {
+      max-height: 100px;
+      opacity: 1;
+    }
+  }
+
+  footer {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+
+  .SocialMediaShareButton,
+  a {
+    cursor: pointer !important;
+    border-radius: 10px;
+    &:focus {
+      background-color: ${colors.linkBackgroundColorTransparent};
+      outline: none;
+      border: none;
+    }
+
+    svg {
+      width: 21px;
+      height: 21px;
+      opacity: 0.95;
+      g {
+        fill: white;
+      }
+    }
+
+    @media (hover), (-moz-touch-enabled: 0) {
+      &:hover {
+        ${Caption} {
+          color: ${colors.linkColor} !important;
+        }
+      }
+    }
+  }
+`;
+
+export default StyledShareBar;
