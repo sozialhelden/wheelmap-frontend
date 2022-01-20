754816b0e59201e46a9033c08152d6e3db87c69e
Julian Scheele <>
2022-01-20 19:53:55 +0100

    refactored to function component

--- src/components/SearchToolbar/SearchInputField.tsx
+++ src/components/SearchToolbar/SearchInputField.tsx
@@ -1,26 +1,78 @@
 import * as React from 'react';
+import { FunctionComponent, useEffect, useRef, useState, KeyboardEvent, forwardRef } from 'react';
 import styled from 'styled-components';
 import { interpolateLab } from 'd3-interpolate';
 import colors from '../../lib/colors';
 import { t } from 'ttag';
 
-type Props = {
-  onSubmit: (event: React.FormEvent<HTMLInputElement>) => void | null,
-  onChange: (value: string) => void,
-  onBlur: () => void | null,
-  onFocus: (event: React.FocusEvent<HTMLInputElement>) => void | null,
-  onClick: (event: React.MouseEvent<HTMLInputElement>) => void | null,
-  ref: (input: HTMLInputElement) => void | null,
-  searchQuery: string | null,
-  className?: string,
-  disabled?: boolean | null,
-  hidden: boolean,
-  ariaRole: string,
+type SearchInputFieldProps = {
+  onSubmit: (event: React.FormEvent<HTMLInputElement>) => void | null;
+  onChange: (value: string) => void;
+  onBlur: () => void | null;
+  onFocus: (event: React.FocusEvent<HTMLInputElement>) => void | null;
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void | null;
+  searchQuery: string | null;
+  hidden: boolean;
+  ariaRole: string;
+  disabled?: boolean | null;
+  className?: string;
 };
 
-type State = {
-  value: string,
-};
+const SearchInputField = forwardRef(
+  (
+    {
+      searchQuery,
+      className,
+      disabled,
+      hidden,
+      ariaRole,
+      onSubmit,
+      onChange,
+      onBlur,
+      onFocus,
+      onClick,
+    }: SearchInputFieldProps,
+    ref: { current: null | HTMLInputElement }
+  ): JSX.Element => {
+    // translator: Placeholder for search input field
+    const defaultPlaceholder = t`Search for place or address`;
+    const [currentSearchInput, setCurrentSearchInput] = useState(searchQuery || '');
+
+    const onKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+      /* Enter */
+      if (event.key === '13' && onSubmit && typeof onSubmit === 'function') {
+        event.preventDefault();
+        onSubmit(event);
+      }
+    };
+
+    const internalOnChange = (event: any) => {
+      const value = event.target.value;
+      setCurrentSearchInput(value);
+      onChange(value);
+    };
+
+    return (
+      <StyledSearchInputField
+        ref={ref}
+        value={currentSearchInput}
+        name="search"
+        onChange={internalOnChange}
+        disabled={disabled}
+        tabIndex={hidden ? -1 : 0}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onClick={onClick}
+        onKeyPress={onKeyPress}
+        className={`search-input ${className || ''}`}
+        placeholder={defaultPlaceholder}
+        aria-label={defaultPlaceholder}
+        role={ariaRole}
+        autoComplete="off"
+      />
+    );
+  }
+);
 
 const StyledSearchInputField = styled.input`
   display: block;
@@ -57,69 +109,4 @@ const StyledSearchInputField = styled.input`
   }
 `;
 
-export default class SearchInputField extends React.Component<Props, State> {
-  input = React.createRef<HTMLInputElement>();
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: props.searchQuery || '',
-    };
-  }
-
-  focus() {
-    this.input.current?.focus();
-  }
-
-  blur() {
-    this.input.current?.blur();
-  }
-
-  clear() {
-    if (this.input.current) {
-      this.input.current.value = '';
-      this.input.current.blur();
-    }
-  }
-
-  keyPressed = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.which === 13 && this.props.onSubmit) {
-      this.props.onSubmit(event);
-      event.preventDefault();
-    }
-  };
-
-  onChange = (event: any) => {
-    const value = event.target.value;
-    this.setState({ value });
-    this.props.onChange(value);
-  };
-
-  render() {
-    const { disabled, hidden, onFocus, onBlur, onClick, className, ariaRole } = this.props;
-    const { value } = this.state;
-    // translator: Placeholder for search input field
-    const defaultPlaceholder = t`Search for place or address`;
-
-    return (
-      <StyledSearchInputField
-        ref={this.input}
-        value={value}
-        name="search"
-        onChange={this.onChange}
-        disabled={disabled}
-        tabIndex={hidden ? -1 : 0}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onClick={onClick}
-        onKeyPress={this.keyPressed}
-        className={`search-input ${className || ''}`}
-        placeholder={defaultPlaceholder}
-        aria-label={defaultPlaceholder}
-        role={ariaRole}
-        autoComplete="off"
-      />
-    );
-  }
-}
+export default SearchInputField;
