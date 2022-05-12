9525085224585d2992c4af420cc20cd90e3a6132
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2022-05-12 11:20:19 +0200

    Remove server-side APM for now

--- src/lib/apm/startServerSideApm.ts
+++ src/lib/apm/startServerSideApm.ts
@@ -1,19 +1,5 @@
-import apmNode from 'elastic-apm-node';
 import env from '../env';
 
 export default function startServerSideApm() {
-  if (!env.ELASTIC_APM_SECRET_TOKEN) {
-    console.log('No secret token given, skipping apm.');
-    return;
-  }
-
-  console.log('Starting server side APM.');
-  const apm = apmNode.start({
-    serviceName: 'wheelmap-react-frontend',
-    serviceVersion: env.npm_package_version, // Used on the APM Server to find the right sourcemap
-    serverUrl: env.ELASTIC_APM_SERVER_URL,
-    secretToken: env.ELASTIC_APM_SECRET_TOKEN,
-  });
-
-  return apm;
+  /* Noop for now. TODO: Implement this :) */
 }
