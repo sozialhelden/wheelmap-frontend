6a44ed14cf967dda1100318c7b9c9cfed15601ee
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2023-07-12 16:09:22 +0200

    Add transit icons

---
+++ src/components/icons/categories/airport_terminal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SvgAirportTerminal = (props: React.SVGAttributes<{}>) => (
+  <svg width="1em" height="1em" viewBox="0 0 15 15" fill="none" {...props}>
+    <path
+      d="M3.171 5.954l.396-.268c.14-.081.29-.104.442-.081l1.374.302L7.106 4.78l-1.84-1.082a.294.294 0 01-.093-.395c.024-.035.047-.07.094-.093l.698-.453c.07-.047.152-.059.221-.047l3.109.663 1.28-.837a3.278 3.278 0 012.748-.396l.256.07a.585.585 0 01.396.733.598.598 0 01-.245.313L6.43 8l-3.2-1.372c-.199-.093-.28-.314-.199-.511a.32.32 0 01.14-.163zM2 12h7v1H2z"
+      fill="#000"
+    />
+  </svg>
+);
+
+export default SvgAirportTerminal;
+
