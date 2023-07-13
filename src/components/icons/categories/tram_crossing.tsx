3b4c1a074e07fa388801b3cf6011cebf9b6e39c9
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2023-07-13 14:47:36 +0200

    Add more category icons

---
+++ src/components/icons/categories/tram_crossing.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SvgTramCrossing = (props: React.SVGAttributes<{}>) => (
+  <svg width="1em" height="1em" viewBox="0 0 15 15" fill="none" {...props}>
+    <g clipPath="url(#tram_crossing_svg__clip0_1940_2041)">
+      <path
+        d="M6 6h-.75C5.11 6 5 5.89 5 5.75v-.5c0-.14.11-.25.25-.25H6V4h-.75C5.11 4 5 3.89 5 3.75v-.5c0-.14.11-.25.25-.25H6V2h-.75C5.11 2 5 1.89 5 1.75v-.5c0-.14.11-.25.25-.25H6V0h1v1h1V0h1v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1h.75c.14 0 .25.11.25.25v.5c0 .14-.11.25-.25.25H9v1H8v-1H7v1H6v-1h-.75c-.14 0-.25-.11-.25-.25v-.5c0-.14.11-.25.25-.25H6v-1h-.75c-.14 0-.25-.11-.25-.25v-.5c0-.14.11-.25.25-.25H6v-1h-.75C5.11 10 5 9.89 5 9.75v-.5c0-.14.11-.25.25-.25H6V8h-.75C5.11 8 5 7.89 5 7.75v-.5c0-.14.11-.25.25-.25H6V6zm2 0H7v1h1V6zm0-1V4H7v1h1zm0 3H7v1h1V8zm0 2H7v1h1v-1zm0 2H7v1h1v-1zM7 3h1V2H7v1zm-7 7V5h4.5v5H0zm10.5 0V5H15v5h-4.5z"
+        fill="#000"
+      />
+    </g>
+    <defs>
+      <clipPath id="tram_crossing_svg__clip0_1940_2041">
+        <path fill="#fff" d="M0 0h15v15H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default SvgTramCrossing;
+
