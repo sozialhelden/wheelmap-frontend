7b65f07381cecba2df4e4ecfcac5d19ca2255a0e
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-02-23 17:50:26 +0100

    Load more places in lower zoom levels
    This fixes that in lower zoom levels, there might not be enough markers loaded to show all filtered markers of a category.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1113,10 +1113,10 @@ export default class Map extends React.Component<Props, State> {
         if (!subCategory) {
           return null;
         }
-        return `${baseUrl}/api/node_types/${subCategory.identifier}/nodes/?api_key=${wheelmapApiKey}&per_page=100&bbox={bbox}&limit=100&t={timestamp}`;
+        return `${baseUrl}/api/node_types/${subCategory.identifier}/nodes/?api_key=${wheelmapApiKey}&per_page=1000&bbox={bbox}&limit=1000&t={timestamp}`;
       }
-      return `${baseUrl}/api/categories/${rootCategory.identifier}/nodes/?api_key=${wheelmapApiKey}&per_page=100&bbox={bbox}&limit=100&t={timestamp}`;
+      return `${baseUrl}/api/categories/${rootCategory.identifier}/nodes/?api_key=${wheelmapApiKey}&per_page=1000&bbox={bbox}&limit=1000&t={timestamp}`;
     }
-    return `${baseUrl}/api/nodes/?api_key=${wheelmapApiKey}&per_page=25&bbox={bbox}&per_page=100&limit=100&t={timestamp}`;
+    return `${baseUrl}/api/nodes/?api_key=${wheelmapApiKey}&per_page=25&bbox={bbox}&per_page=1000&limit=1000&t={timestamp}`;
   }
 }
