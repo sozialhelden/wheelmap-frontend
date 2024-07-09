98c3e1479560b88d4af943f7cb09b9d5a6b5155a
Anas Alhakim <anasalhakim@gmail.com>
2024-07-09 13:48:30 +0200

    Update Children Prio in PlaceAccessibility.

--- src/components/NodeToolbar/AccessibilitySection/PlaceAccessibilitySection.tsx
+++ src/components/NodeToolbar/AccessibilitySection/PlaceAccessibilitySection.tsx
@@ -1,29 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 
-import StyledFrame from './StyledFrame';
-import AccessibilityDetailsTree from './AccessibilityDetailsTree';
-import AccessibleDescription from './AccessibleDescription';
-import EditFormSubmissionButton from '../AccessibilityEditor/EditFormSubmissionButton';
-import AccessibilitySourceDisclaimer from './AccessibilitySourceDisclaimer';
-import WheelchairAndToiletAccessibility from './WheelchairAndToiletAccessibility';
+import EditFormSubmissionButton from "../AccessibilityEditor/EditFormSubmissionButton";
+import AccessibilityDetailsTree from "./AccessibilityDetailsTree";
+import AccessibilitySourceDisclaimer from "./AccessibilitySourceDisclaimer";
+import AccessibleDescription from "./AccessibleDescription";
+import StyledFrame from "./StyledFrame";
+import WheelchairAndToiletAccessibility from "./WheelchairAndToiletAccessibility";
 
-import { SourceWithLicense } from '../../../app/PlaceDetailsProps';
-import {
-  Feature,
-  isWheelmapProperties,
-  isWheelchairAccessible,
-  AccessibilityCloudProperties,
-  isWheelmapFeature,
-} from '../../../lib/Feature';
-import { YesNoLimitedUnknown } from '../../../lib/Feature';
-import { Category } from '../../../lib/Categories';
-import filterAccessibility from '../../../lib/filterAccessibility';
+import { SourceWithLicense } from "../../../app/PlaceDetailsProps";
+import { Category } from "../../../lib/Categories";
+import { AccessibilityCloudProperties, Feature, isWheelchairAccessible, isWheelmapFeature, isWheelmapProperties, YesNoLimitedUnknown } from "../../../lib/Feature";
+import filterAccessibility from "../../../lib/filterAccessibility";
 
-import Description from './Description';
-import AppContext from '../../../AppContext';
-import isA11yEditable, { hasDefaultOSMDescription } from '../AccessibilityEditor/isA11yEditable';
-import { useAccessibilityAttributes } from '../../../lib/data-fetching/useAccessibilityAttributes';
-import { translatedStringFromObject } from '../../../lib/i18n';
+import AppContext from "../../../AppContext";
+import { useAccessibilityAttributes } from "../../../lib/data-fetching/useAccessibilityAttributes";
+import isA11yEditable, { hasDefaultOSMDescription } from "../AccessibilityEditor/isA11yEditable";
+import Description from "./Description";
 
 type Props = {
   featureId: string | number | null;
@@ -47,9 +39,7 @@ export default function PlaceAccessibilitySection(props: Props) {
   const appContext = React.useContext(AppContext);
   const properties = feature && feature.properties;
 
-  const { data: accessibilityAttributes, error } = useAccessibilityAttributes([
-    appContext.preferredLanguage,
-  ]);
+  const { data: accessibilityAttributes, error } = useAccessibilityAttributes([appContext.preferredLanguage]);
   if (error) {
     throw error;
   }
@@ -61,34 +51,16 @@ export default function PlaceAccessibilitySection(props: Props) {
   const primarySource = sources.length > 0 ? sources[0].source : undefined;
   const isEditingEnabled = isA11yEditable(feature, appContext.app, primarySource);
 
-  const accessibilityTree =
-    accessibilityAttributes &&
-    properties &&
-    !isWheelmapProperties(properties) &&
-    typeof properties.accessibility === 'object'
-      ? properties.accessibility
-      : null;
-  const filteredAccessibilityTree = accessibilityTree
-    ? filterAccessibility(accessibilityTree)
-    : null;
-  const accessibilityDetailsTree = filteredAccessibilityTree && (
-    <AccessibilityDetailsTree
-      details={filteredAccessibilityTree}
-      isNested={true}
-      accessibilityAttributes={accessibilityAttributes}
-    />
-  );
+  const accessibilityTree = accessibilityAttributes && properties && !isWheelmapProperties(properties) && typeof properties.accessibility === "object" ? properties.accessibility : null;
+  const filteredAccessibilityTree = accessibilityTree ? filterAccessibility(accessibilityTree) : null;
+  const accessibilityDetailsTree = filteredAccessibilityTree && <AccessibilityDetailsTree details={filteredAccessibilityTree} isNested={true} accessibilityAttributes={accessibilityAttributes} />;
   let description: string = null;
-  if (
-    properties &&
-    isWheelmapProperties(properties) &&
-    typeof properties.wheelchair_description === 'string'
-  ) {
+  if (properties && isWheelmapProperties(properties) && typeof properties.wheelchair_description === "string") {
     description = properties.wheelchair_description;
   }
   const descriptionElement = description ? <Description>{description}</Description> : null;
 
-  if (isWheelmapFeature(feature) && !description && isWheelchairAccessible(properties) === 'unknown') {
+  if (isWheelmapFeature(feature) && !description && isWheelchairAccessible(properties) === "unknown") {
     return null;
   }
 
@@ -96,28 +68,13 @@ export default function PlaceAccessibilitySection(props: Props) {
 
   return (
     <StyledFrame noseOffsetX={cluster ? 67 : undefined}>
-      <WheelchairAndToiletAccessibility
-        isEditingEnabled={isEditingEnabled}
-        feature={feature}
-        toiletsNearby={toiletsNearby}
-        onOpenWheelchairAccessibility={props.onOpenWheelchairAccessibility}
-        onOpenToiletAccessibility={props.onOpenToiletAccessibility}
-        onOpenToiletNearby={props.onOpenToiletNearby}
-        showDescription={showDefaultDescription}
-      />
+      <WheelchairAndToiletAccessibility isEditingEnabled={isEditingEnabled} feature={feature} toiletsNearby={toiletsNearby} onOpenWheelchairAccessibility={props.onOpenWheelchairAccessibility} onOpenToiletAccessibility={props.onOpenToiletAccessibility} onOpenToiletNearby={props.onOpenToiletNearby} showDescription={showDefaultDescription} />
       {description && descriptionElement}
       <AccessibleDescription properties={properties} />
       <EditFormSubmissionButton featureId={featureId} feature={feature} sources={sources} />
-      {accessibilityDetailsTree}
-
-      {!isWheelmapFeature && (
-        <AccessibilitySourceDisclaimer
-          properties={properties as AccessibilityCloudProperties}
-          appToken={appContext.app.tokenString}
-        />
-      )}
-
       {children}
+      {accessibilityDetailsTree}
+      {!isWheelmapFeature && <AccessibilitySourceDisclaimer properties={properties as AccessibilityCloudProperties} appToken={appContext.app.tokenString} />}
     </StyledFrame>
   );
 }
