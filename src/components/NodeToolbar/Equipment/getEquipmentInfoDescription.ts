3772b69b59894e0ff73aeb2e0e060e4580433f31
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-07-20 23:48:28 +0200

    Fix potential crash in EquipmentInfo description generation

--- src/components/NodeToolbar/Equipment/getEquipmentInfoDescription.ts
+++ src/components/NodeToolbar/Equipment/getEquipmentInfoDescription.ts
@@ -1,22 +1,23 @@
-import { translatedStringFromObject, LocalizedString } from '../../../lib/i18n';
 import { EquipmentInfo } from '../../../lib/EquipmentInfo';
+import { LocalizedString, translatedStringFromObject } from '../../../lib/i18n';
 
 export default function getEquipmentInfoDescription(
   equipmentInfo: EquipmentInfo | null | undefined,
   variant: 'shortDescription' | 'longDescription' | 'description'
 ) {
-  if (!equipmentInfo) {
+  if (!equipmentInfo || !equipmentInfo.properties) {
     return;
   }
 
   const variantsToTry = [variant, 'shortDescription', 'description', 'longDescription'];
   let description: LocalizedString | undefined = undefined;
   for (const variant of variantsToTry) {
-    const d = equipmentInfo.properties[variant];
+    const d = equipmentInfo.properties?.[variant];
     if (d) {
       description = d;
       break;
     }
   }
+
   return description && translatedStringFromObject(description);
 }
