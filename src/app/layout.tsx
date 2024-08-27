f6f683ef0a1cf585bcca9e31ac39c6b147f6c876
Henrik Hinrichs <henrik@mutaphysis.de>
2024-08-27 14:18:14 +0200

    Fix integration of styled-components with nextjs
    https://nextjs.org/docs/app/building-your-application/styling/css-in-js#styled-components

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+import StyledComponentsRegistry from '../lib/context/Registry'
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
