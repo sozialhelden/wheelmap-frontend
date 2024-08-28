ff239a908a33859e400e2c7b260037896cd36b39
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-08-28 16:32:35 +0200

    Add `fetchWithTypeTag` function

---
+++ src/lib/model/ac/fetchWithTypeTag.tsx
@@ -0,0 +1,15 @@
+import { AccessibilityCloudTypeMapping } from "../typing/AccessibilityCloudTypeMapping";
+import { HasTypeTag } from "../typing/TypeTaggedWithId";
+
+
+export async function fetchWithTypeTag<S extends keyof AccessibilityCloudTypeMapping>(
+  typeName: S,
+  url: string
+): Promise<HasTypeTag<S>> {
+  const r = await fetch(url);
+  if (!r.ok) {
+    throw new Error(`Failed to fetch \`${typeName}\` (${r.status} ${r.statusText}) from ${url}`);
+  }
+  const json = await r.json();
+  return { '@type': typeName, ...json };
+}
