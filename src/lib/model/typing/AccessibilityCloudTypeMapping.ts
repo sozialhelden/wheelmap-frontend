ae3b7cdb4cee67e2ab23f0ffb10c963d8cf4b8eb
Henrik Hinrichs <henrik@mutaphysis.de>
2024-09-18 11:16:50 +0200

    Slightly improve feature tagging

--- src/lib/model/typing/AccessibilityCloudTypeMapping.ts
+++ src/lib/model/typing/AccessibilityCloudTypeMapping.ts
@@ -1,10 +1,15 @@
 import { Entrance, EquipmentInfo, PlaceInfo } from '@sozialhelden/a11yjson'
 import { IApp } from '../ac/App'
 import ISource from '../ac/ISource'
-import { MappingEvent } from '../ac/MappingEvent';
-import IAccessibilityAttribute from '../ac/IAccessibilityAttribute';
-import { memoizedKebabCase } from '../../util/strings/memoizedKebabCase';
-import { memoizedPluralize } from '../ac/pluralize';
+import { MappingEvent } from '../ac/MappingEvent'
+import IAccessibilityAttribute from '../ac/IAccessibilityAttribute'
+import { memoizedKebabCase } from '../../util/strings/memoizedKebabCase'
+import { memoizedPluralize } from '../ac/pluralize'
+
+export interface A11yJSONTypeMapping {
+  'a11yjson:EquipmentInfo': EquipmentInfo;
+  'a11yjson:PlaceInfo': PlaceInfo;
+}
 
 export interface AccessibilityCloudTypeMapping {
   'ac:EquipmentInfo': EquipmentInfo;
@@ -16,7 +21,7 @@ export interface AccessibilityCloudTypeMapping {
   'ac:AccessibilityAttribute': IAccessibilityAttribute;
 }
 
-export type AccessibilityCloudRDFType = keyof AccessibilityCloudTypeMapping;
+export type AccessibilityCloudRDFType = keyof AccessibilityCloudTypeMapping
 
 /**
  * Returns the kebab-cased plural name (example: `"place-infos"`) of the collection for a given RDF
@@ -29,5 +34,5 @@ export function getAccessibilityCloudCollectionName(rdfType: AccessibilityCloudR
   }
   const kebabName = memoizedKebabCase(nameWithoutPrefix)
   const kebabPluralName = memoizedPluralize(kebabName)
-  return kebabPluralName;
+  return kebabPluralName
 }
--- src/lib/model/typing/TypeTaggedWithId.ts
+++ src/lib/model/typing/TypeTaggedWithId.ts
@@ -1,9 +1,9 @@
-import { AccessibilityCloudTypeMapping } from './AccessibilityCloudTypeMapping'
+import { A11yJSONTypeMapping, AccessibilityCloudTypeMapping } from './AccessibilityCloudTypeMapping'
 import { KomootTypeMapping } from './KomootTypeMapping'
 import { OSMTypeMapping } from './OSMTypeMapping'
 
-export type TypeMapping = AccessibilityCloudTypeMapping & OSMTypeMapping & KomootTypeMapping;
-export type KnownTypeString = keyof TypeMapping;
-export type HasTypeTag<S extends keyof TypeMapping> = { ['@type']: S; };
-export type TypeTagged<S extends keyof TypeMapping> = TypeMapping[S] & HasTypeTag<S>;
-export type TypeTaggedWithId<S extends keyof TypeMapping> = TypeTagged<S> & { _id: string; };
+export type TypeMapping = AccessibilityCloudTypeMapping & OSMTypeMapping & KomootTypeMapping & A11yJSONTypeMapping
+export type KnownTypeString = keyof TypeMapping
+export type HasTypeTag<S extends keyof TypeMapping> = { ['@type']: S; }
+export type TypeTagged<S extends keyof TypeMapping> = TypeMapping[S] & HasTypeTag<S>
+export type TypeTaggedWithId<S extends keyof TypeMapping> = TypeTagged<S> & { _id: string; }
