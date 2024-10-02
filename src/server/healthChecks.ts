66bcf0e3768f7485c5a617516bbbf9bc3dc50d96
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-10-02 11:08:28 +0200

    Respond 200 OK to /ping

--- src/server/healthChecks.ts
+++ src/server/healthChecks.ts
@@ -5,6 +5,9 @@ export default function registerHealthChecks(app) {
   app.get('/healthz', (req, res) => {
     res.status(200).send('OK!');
   });
+  app.get('/ping', (req, res) => {
+    res.status(200).send('OK!');
+  });
   app.get('/ready', (req, res) => {
     // TODO: Add logic for overload here
     res.status(200).send('OK!');
