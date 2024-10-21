d47b912442458420575a6249dfb7116aa3580326
Mayaryin <lynn.janzen@posteo.de>
2024-10-21 16:59:14 +0200

    Comment out unused code to avoid deployment failure

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -1,5 +1,5 @@
 import { Locale } from "./Locale";
 import { localeFromString } from "./localeFromString";
 
-const systemLocale = navigator.language || "de-De";
-export const currentLocales: Locale[] = [systemLocale, systemLocale.split('-')[0]].map(localeFromString);
+//const systemLocale = navigator.language || "de-De";
+//export const currentLocales: Locale[] = [systemLocale, systemLocale.split('-')[0]].map(localeFromString);
