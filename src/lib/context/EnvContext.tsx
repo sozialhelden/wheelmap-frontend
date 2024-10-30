6d243fc131513a7d6d14c27cfd0adae8a7d328b0
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-10-30 14:06:04 +0100

    Not-so-beautiful fix for missing environment variables in EnvContext

--- src/lib/context/EnvContext.tsx
+++ src/lib/context/EnvContext.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export type EnvironmentVariables = Record<string, string | undefined>;
-const EnvContext = React.createContext<EnvironmentVariables>({})
+export type EnvironmentVariables = Record<string, string | undefined>
+const EnvContext = React.createContext<EnvironmentVariables>(global.process ? process.env : {})
 
 export default EnvContext
 
 export function useEnvContext() {
-  return React.useContext(EnvContext)
+  const envFromContext = React.useContext(EnvContext)
+  return (global.process) ? process.env : envFromContext
 }
