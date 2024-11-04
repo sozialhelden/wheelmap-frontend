e525fe0d3a5f0fb31ea8cb35106c7b998c07a295
Mayaryin <lynn.janzen@posteo.de>
2024-11-04 19:18:18 +0100

    add state setter and click handler function to base editor props to enable passing of component state up to "parent" component

--- src/components/CombinedFeaturePanel/editors/BaseEditor.tsx
+++ src/components/CombinedFeaturePanel/editors/BaseEditor.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { AnyFeature } from '../../../lib/model/geo/AnyFeature'
+import { EditorTagValue } from './EditorTagValue'
 
 export type BaseEditorProps = {
   tagKey: string
   feature: AnyFeature
   onUrlMutationSuccess: (urls: string[]) => void
+  setParentState: (tagValue: EditorTagValue) => void
+  handleSubmitButtonClick: () => void
 }
 
 export type BaseEditorComponent = React.FC<BaseEditorProps>
