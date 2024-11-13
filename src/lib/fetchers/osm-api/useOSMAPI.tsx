0b34f4fcbfb599a4a03ef8ad356c6f39b9f7a0c9
Henrik Hinrichs <henrik@mutaphysis.de>
2024-11-13 11:16:56 +0100

    fix: remove spammy log

--- src/lib/fetchers/osm-api/useOSMAPI.tsx
+++ src/lib/fetchers/osm-api/useOSMAPI.tsx
@@ -3,7 +3,7 @@ import { useEnvContext } from '../../context/EnvContext'
 
 export default function useOSMAPI({ cached = true }: { cached: boolean }) {
   const env = useEnvContext()
-  console.log('env: ', env)
+  // console.log('env: ', env)
   const appToken = useCurrentAppToken() || env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_APP_TOKEN
   const baseUrl = cached ? env.NEXT_PUBLIC_OSM_API_TILE_BACKEND_URL?.replace(/\{n\}/, '1') : env.NEXT_PUBLIC_OSM_API_BACKEND_URL
   if (!baseUrl) {
