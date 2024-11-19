e5eda03c898d64553234333667ce5d0ed5d5b442
Mayaryin <lynn.janzen@posteo.de>
2024-11-19 13:48:45 +0100

    remove empty object

--- src/components/CombinedFeaturePanel/components/AccessibilitySection/AddWheelchairDescription.tsx
+++ src/components/CombinedFeaturePanel/components/AccessibilitySection/AddWheelchairDescription.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { EditButton } from './EditButton'
 import { FeaturePanelContext } from '../../FeaturePanelContext'
 
-export default function AddWheelchairDescription({}) {
+export default function AddWheelchairDescription() {
   const { baseFeatureUrl } = useContext(FeaturePanelContext)
   const key = 'wheelchair:description'
   const editURL = `${baseFeatureUrl}/edit/${key}`
