bfe8ab451a056856fd79a1b03af1ab570a59e45a
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-11-29 18:15:38 +0100

    Add emergency tag support

--- src/lib/model/osm/tag-config/horizontalKeys.ts
+++ src/lib/model/osm/tag-config/horizontalKeys.ts
@@ -14,4 +14,5 @@ export const horizontalKeys = new Set([
   'internet',
   'means_of_transport',
   'amenities',
+  'emergency',
 ])
--- src/lib/model/osm/tag-config/tagsWithSemicolonSupport.ts
+++ src/lib/model/osm/tag-config/tagsWithSemicolonSupport.ts
@@ -24,4 +24,5 @@ export const tagsWithSemicolonSupport = [
   'charge',
   'material',
   'building:material',
+  'emergency',
 ]
