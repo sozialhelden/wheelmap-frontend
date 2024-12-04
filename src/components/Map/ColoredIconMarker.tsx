483d5b90043cc8462ab5b48c7ef315e8be52ea3c
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-12-04 16:13:50 +0100

    Add halo around icon marker backgrounds

--- src/components/Map/ColoredIconMarker.tsx
+++ src/components/Map/ColoredIconMarker.tsx
@@ -10,15 +10,16 @@ export default function ColoredIconMarker(props: React.SVGAttributes<{}> & Props
   return (
   // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg width="1em" height="1em" viewBox="0 0 25 25" {...props}>
-      {accessibilityGrade === 'yes' && <circle cx={12.5} cy={12.5} r={10.5} fill="#7EC512" fillRule="nonzero" />}
+      {accessibilityGrade === 'yes' && <circle cx={12.5} cy={12.5} r={10.5} fill="#7EC512" fillRule="nonzero" filter="url(#halo)" />}
       {accessibilityGrade === 'limited' && (
         <path
           fill="#FC9B32"
           d="M6.743 2.364h11.55l5.776 10.003-5.775 10.003H6.743L.967 12.367z"
           fillRule="evenodd"
+          filter="url(#halo)"
         />
       )}
-      {accessibilityGrade === 'no' && <path d="M22 22V3.072H3v19z" fill="#F54B4B" fillRule="nonzero" />}
+      {accessibilityGrade === 'no' && <path d="M22 22V3.072H3v19z" fill="#F54B4B" fillRule="nonzero" filter="url(#halo)" />}
       {accessibilityGrade === 'unknown' && (
         <path
           strokeOpacity={0.25}
@@ -26,8 +27,20 @@ export default function ColoredIconMarker(props: React.SVGAttributes<{}> & Props
           d="M.707 12.5l11.794 11.793 11.798-11.79L12.5.707.707 12.5z"
           fill="#efefef"
           fillRule="evenodd"
+          filter="url(#halo)"
         />
       )}
+      <defs>
+        <filter id="halo">
+          <feDropShadow
+            dx={0}
+            dy={0}
+            stdDeviation={2}
+            floodColor="white"
+            floodOpacity={1}
+          />
+        </filter>
+      </defs>
       {children}
     </svg>
   )
