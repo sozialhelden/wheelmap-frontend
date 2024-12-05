dd189a445af9ad297343e6b455434d3103c6d7ed
Kris Siepert <m@kris.cool>
2024-12-05 16:23:39 +0100

    fix: image single view not rendering

--- src/pages/[placeType]/[id]/images/[imageId]/index.tsx
+++ src/pages/[placeType]/[id]/images/[imageId]/index.tsx
@@ -1,16 +1,17 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { useRouter } from 'next/router'
 import { CombinedFeaturePanel } from '../../../../../components/CombinedFeaturePanel/CombinedFeaturePanel'
 import { FeaturePanelContext } from '../../../../../components/CombinedFeaturePanel/FeaturePanelContext'
-import { getLayout } from '../../../../../components/App/MapLayout'
+import { getLayout } from '../../../../../components/CombinedFeaturePanel/PlaceLayout'
 
 export default function ShowImagePage() {
   const { features } = useContext(FeaturePanelContext)
+  const resolvedFeatures = useMemo(() => features.map(({ feature }) => feature?.requestedFeature).filter((x) => !!x), [features])
   const { query: { imageId } } = useRouter()
 
   const id = typeof imageId === 'string' ? imageId : imageId[0]
   return (
-    <CombinedFeaturePanel features={features} focusImage={id} />
+    <CombinedFeaturePanel features={resolvedFeatures} focusImage={id} />
   )
 }
 
