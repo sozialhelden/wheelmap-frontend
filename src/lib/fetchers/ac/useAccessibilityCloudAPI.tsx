98792c7f55b42b1286b4346dd874efcf5ab4df7b
Kris Siepert <m@kris.cool>
2024-12-10 14:49:01 +0100

    chore: make sure a11ycloud app token and base url are set
    This helps with typing down the line, as appToken as well as baseUrl are
    always string and consumers don't need to handle the undefined case.

--- src/lib/fetchers/ac/useAccessibilityCloudAPI.tsx
+++ src/lib/fetchers/ac/useAccessibilityCloudAPI.tsx
@@ -1,14 +1,34 @@
-import { useCurrentAppToken } from '../../context/AppContext'
-import { EnvironmentVariables, useEnvContext } from '../../context/EnvContext'
+import { useCurrentAppToken } from "../../context/AppContext";
+import {
+  type EnvironmentVariables,
+  useEnvContext,
+} from "../../context/EnvContext";
 
-export function getAccessibilityCloudAPI(env: EnvironmentVariables, currentAppToken: string, cached: boolean) {
-  const appToken = currentAppToken || env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_APP_TOKEN
-  const baseUrl = cached ? env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_BASE_URL : env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_UNCACHED_BASE_URL
-  return { baseUrl, appToken }
+export function getAccessibilityCloudAPI(
+  env: EnvironmentVariables,
+  currentAppToken: string,
+  cached: boolean,
+) {
+  const appToken =
+    currentAppToken || env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_APP_TOKEN;
+  const baseUrl = cached
+    ? env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_BASE_URL
+    : env.NEXT_PUBLIC_ACCESSIBILITY_CLOUD_UNCACHED_BASE_URL;
+
+  if (!appToken) {
+    throw new Error("Accessibility Cloud API token not set");
+  }
+  if (!baseUrl) {
+    throw new Error("Accessibility Cloud base url not set");
+  }
+
+  return { baseUrl, appToken };
 }
 
-export default function useAccessibilityCloudAPI({ cached = true }: { cached: boolean }) {
-  const env = useEnvContext()
-  const currentAppToken = useCurrentAppToken()
-  return getAccessibilityCloudAPI(env, currentAppToken, cached)
+export default function useAccessibilityCloudAPI({
+  cached = true,
+}: { cached: boolean }) {
+  const env = useEnvContext();
+  const currentAppToken = useCurrentAppToken();
+  return getAccessibilityCloudAPI(env, currentAppToken, cached);
 }
