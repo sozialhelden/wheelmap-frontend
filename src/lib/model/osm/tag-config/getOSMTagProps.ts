f65dfe7ee5638fe5f04b62890f726e9ae3f5a739
Mayaryin <lynn.janzen@posteo.de>
2024-12-16 16:27:37 +0100

    insert pseudo tag value

--- src/lib/model/osm/tag-config/getOSMTagProps.ts
+++ src/lib/model/osm/tag-config/getOSMTagProps.ts
@@ -1,67 +1,83 @@
-import React from 'react'
+import type React from "react";
+import type { OSMTagProps } from "../../../../components/CombinedFeaturePanel/components/AccessibilitySection/OSMTagProps";
+import { valueRenderFunctions } from "../../../../components/CombinedFeaturePanel/components/AccessibilitySection/valueRenderFunctions";
+import { getLocalizedStringTranslationWithMultipleLocales as localize } from "../../../i18n/getLocalizedStringTranslationWithMultipleLocales";
+import type IAccessibilityAttribute from "../../ac/IAccessibilityAttribute";
 import {
-  getLocalizedStringTranslationWithMultipleLocales as localize,
-} from '../../../i18n/getLocalizedStringTranslationWithMultipleLocales'
-import IAccessibilityAttribute from '../../ac/IAccessibilityAttribute'
-import { OSMTagProps } from '../../../../components/CombinedFeaturePanel/components/AccessibilitySection/OSMTagProps'
-import { horizontalKeys } from './horizontalKeys'
-import { languageTaggedKeys } from './languageTaggedKeys'
-import { additionalPrefixesForKeys } from './sidewalkPrefixSet'
-import { editableKeys } from './editableKeys'
-import { tagsWithoutDisplayedKeyRegExp } from './tagsWithoutDisplayedKeyRegExp'
-import { tagsWithoutDisplayedKeySet } from './tagsWithoutDisplayedKeySet'
-import { valueRenderFunctions } from '../../../../components/CombinedFeaturePanel/components/AccessibilitySection/valueRenderFunctions'
-import { AnyFeature, isOSMFeature, isPlaceInfo } from '../../geo/AnyFeature'
+  type AnyFeature,
+  isOSMFeature,
+  isPlaceInfo,
+} from "../../geo/AnyFeature";
+import { editableKeys } from "./editableKeys";
+import { horizontalKeys } from "./horizontalKeys";
+import { languageTaggedKeys } from "./languageTaggedKeys";
+import { additionalPrefixesForKeys } from "./sidewalkPrefixSet";
+import { tagsWithoutDisplayedKeyRegExp } from "./tagsWithoutDisplayedKeyRegExp";
+import { tagsWithoutDisplayedKeySet } from "./tagsWithoutDisplayedKeySet";
 
 function findAttribute(
   attributesById: Map<string, IAccessibilityAttribute>,
   key: string,
   value?: string,
 ): IAccessibilityAttribute | undefined {
-  const suffix = key.match('[^:]+$')?.[0]
-  const prefixes = [null, ...(additionalPrefixesForKeys.get(suffix) || [])] || [null]
+  const suffix = key.match("[^:]+$")?.[0];
+  const prefixes = [null, ...(additionalPrefixesForKeys.get(suffix) || [])] || [
+    null,
+  ];
   for (const prefix of prefixes) {
-    const searchKey = prefix ? `osm:${prefix}:${suffix}` : `osm:${key}`
-    const attribute = attributesById?.get(value ? `${searchKey}=${value}` : searchKey)
-      || attributesById?.get(value ? `osm:${suffix}=${value}` : `osm:${suffix}`)
+    let searchKey = prefix ? `osm:${prefix}:${suffix}` : `osm:${key}`;
+    // addWheelchairDescription is a pseudo tag that is added to the tag list as a kind of placeholder
+    // so that a call to action and an edit button can be displayed in case no wheelchair description
+    // is present in the feature
+    if (key === "addWheelchairDescription") {
+      searchKey = key;
+    }
+    const attribute =
+      attributesById?.get(value ? `${searchKey}=${value}` : searchKey) ||
+      attributesById?.get(value ? `osm:${suffix}=${value}` : `osm:${suffix}`);
     if (attribute) {
-      return attribute
+      return attribute;
     }
   }
-  return undefined
+  return undefined;
 }
 
-export function getOSMTagProps(
-  {
-    key, matchedKey, singleValue, attributesById, languageTags,
-    feature, baseFeatureUrl,
-  }: {
-    key: string;
-    matchedKey: string;
-    singleValue: string;
-    attributesById: Map<string, IAccessibilityAttribute>;
-    languageTags: string[];
-    feature: AnyFeature;
-    baseFeatureUrl: string;
-  },
-): OSMTagProps {
-  const keyWithoutLanguageTag = key.replace(/:\w\w(?:[_-]\w\w)?$/, '')
-  const keyAttribute = findAttribute(attributesById, key)
-    || languageTaggedKeys.has(keyWithoutLanguageTag)
-    && findAttribute(attributesById, keyWithoutLanguageTag)
-  const valueAttribute = findAttribute(attributesById, key, singleValue)
-  let valueLabel: string | React.ReactNode | undefined
+export function getOSMTagProps({
+  key,
+  matchedKey,
+  singleValue,
+  attributesById,
+  languageTags,
+  feature,
+  baseFeatureUrl,
+}: {
+  key: string;
+  matchedKey: string;
+  singleValue: string;
+  attributesById: Map<string, IAccessibilityAttribute>;
+  languageTags: string[];
+  feature: AnyFeature;
+  baseFeatureUrl: string;
+}): OSMTagProps {
+  const keyWithoutLanguageTag = key.replace(/:\w\w(?:[_-]\w\w)?$/, "");
+  const keyAttribute =
+    findAttribute(attributesById, key) ||
+    (languageTaggedKeys.has(keyWithoutLanguageTag) &&
+      findAttribute(attributesById, keyWithoutLanguageTag));
+  const valueAttribute = findAttribute(attributesById, key, singleValue);
+  let valueLabel: string | React.ReactNode | undefined;
 
-  const matches = matchedKey ? key.match(matchedKey) : undefined
+  const matches = matchedKey ? key.match(matchedKey) : undefined;
 
-  const valueRenderFn = valueRenderFunctions[matchedKey]
-  const defaultValueLabel = localize(valueAttribute?.label, languageTags)
-    || localize(valueAttribute?.shortLabel, languageTags)
-    || singleValue
+  const valueRenderFn = valueRenderFunctions[matchedKey];
+  const defaultValueLabel =
+    localize(valueAttribute?.label, languageTags) ||
+    localize(valueAttribute?.shortLabel, languageTags) ||
+    singleValue;
 
   if (valueRenderFn) {
-    const osmFeature = isOSMFeature(feature) ? feature : null
-    const accessibilityCloudFeature = isPlaceInfo(feature) ? feature : null
+    const osmFeature = isOSMFeature(feature) ? feature : null;
+    const accessibilityCloudFeature = isPlaceInfo(feature) ? feature : null;
     valueLabel = valueRenderFn({
       value: singleValue,
       matches,
@@ -69,28 +85,33 @@ export function getOSMTagProps(
       osmFeature,
       accessibilityCloudFeature,
       defaultValueLabel,
-    })
+    });
   } else {
-    valueLabel = defaultValueLabel
+    valueLabel = defaultValueLabel;
   }
-  const valueSummary = localize(valueAttribute?.summary, languageTags)
-  const valueDetails = localize(valueAttribute?.details, languageTags) || (!valueLabel && valueSummary)
+  const valueSummary = localize(valueAttribute?.summary, languageTags);
+  const valueDetails =
+    localize(valueAttribute?.details, languageTags) ||
+    (!valueLabel && valueSummary);
 
-  const keyLabel = localize(keyAttribute?.shortLabel, languageTags)
-    || localize(keyAttribute?.label, languageTags)
-    || key
-  const keySummary = localize(keyAttribute?.summary, languageTags)
-  const keyDetails = localize(keyAttribute?.details, languageTags) || (!keyLabel && keySummary)
-  const suffix = key.match('[^:]+$')?.[0]
-  const hasDisplayedKey = !!keyLabel
-    && !(
-      tagsWithoutDisplayedKeySet.has(key)
-      || tagsWithoutDisplayedKeySet.has(suffix)
-      || tagsWithoutDisplayedKeyRegExp.test(key)
-    )
+  const keyLabel =
+    localize(keyAttribute?.shortLabel, languageTags) ||
+    localize(keyAttribute?.label, languageTags) ||
+    key;
+  const keySummary = localize(keyAttribute?.summary, languageTags);
+  const keyDetails =
+    localize(keyAttribute?.details, languageTags) || (!keyLabel && keySummary);
+  const suffix = key.match("[^:]+$")?.[0];
+  const hasDisplayedKey =
+    !!keyLabel &&
+    !(
+      tagsWithoutDisplayedKeySet.has(key) ||
+      tagsWithoutDisplayedKeySet.has(suffix) ||
+      tagsWithoutDisplayedKeyRegExp.test(key)
+    );
 
-  const isEditable = editableKeys.has(key)
-  const editURL = `${baseFeatureUrl}/edit/${key}`
+  const isEditable = editableKeys.has(key);
+  const editURL = `${baseFeatureUrl}/edit/${key}`;
   const tagProps: OSMTagProps = {
     tagKey: key,
     hasDisplayedKey,
@@ -103,6 +124,6 @@ export function getOSMTagProps(
     valueDetails,
     keyDetails,
     isHorizontal: horizontalKeys.has(key),
-  }
-  return tagProps
+  };
+  return tagProps;
 }
