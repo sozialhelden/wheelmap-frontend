673693d2013fffb02321e2cc0bdd1bd3b27c2a60
Mayaryin <lynn.janzen@posteo.de>
2024-12-16 16:26:06 +0100

    remove pseudo key label

--- src/components/CombinedFeaturePanel/components/AccessibilitySection/OSMTagTableRowOrListElement.tsx
+++ src/components/CombinedFeaturePanel/components/AccessibilitySection/OSMTagTableRowOrListElement.tsx
@@ -1,10 +1,10 @@
+import { Tooltip } from "@radix-ui/themes";
 import styled from "styled-components";
+import { horizontalKeys } from "../../../../lib/model/osm/tag-config/horizontalKeys";
 import StyledMarkdown from "../../../shared/StyledMarkdown";
 import { EditButton } from "./EditButton";
 import type { OSMTagProps } from "./OSMTagProps";
-import { horizontalKeys } from "../../../../lib/model/osm/tag-config/horizontalKeys";
 import StyledTag from "./StyledTag";
-import { Tooltip } from "@radix-ui/themes";
 
 const StyledListElement = styled.li`
   list-style: none;
@@ -22,120 +22,116 @@ const StyledListElement = styled.li`
 `;
 
 export function OSMTagTableRowOrListElement({
-	tagKey,
-	hasDisplayedKey,
-	keyLabel,
-	valueElement,
-	isEditable,
-	editURL,
-	keyDetails,
-	valueDetails,
-	isHorizontal,
+  tagKey,
+  hasDisplayedKey,
+  keyLabel,
+  valueElement,
+  isEditable,
+  editURL,
+  keyDetails,
+  valueDetails,
+  isHorizontal,
 }: OSMTagProps) {
-	// Baseline alignment for keys that display a list of horizontal tags
-	const keyStyle = horizontalKeys.has(tagKey) ? { paddingTop: "0.55rem" } : {};
+  // Baseline alignment for keys that display a list of horizontal tags
+  const keyStyle = horizontalKeys.has(tagKey) ? { paddingTop: "0.55rem" } : {};
 
-  if (tagKey === 'add_wheelchair_description') {
-    keyLabel = 'Add a text description for wheelchair users.'
-  }
+  const HeaderElement = isHorizontal ? "header" : "th";
 
-	const HeaderElement = isHorizontal ? "header" : "th";
+  const displayedKey = hasDisplayedKey && (
+    <HeaderElement rowSpan={keyDetails ? 1 : 2} style={keyStyle}>
+      {keyLabel}
+    </HeaderElement>
+  );
 
-	const displayedKey = hasDisplayedKey && (
-		<HeaderElement rowSpan={keyDetails ? 1 : 2} style={keyStyle}>
-			{keyLabel}
-		</HeaderElement>
-	);
+  const detailElements = [keyDetails, valueDetails].filter(Boolean);
+  const hasDetails = detailElements.length > 0;
 
-	const detailElements = [keyDetails, valueDetails].filter(Boolean);
-	const hasDetails = detailElements.length > 0;
+  const detailElementsContained = hasDetails && (
+    <div style={{ maxWidth: "30rem", padding: ".5rem" }}>
+      {detailElements.length === 1 && (
+        <StyledMarkdown>{detailElements[0]}</StyledMarkdown>
+      )}
+      {detailElements.length > 1 && (
+        <ul>
+          {detailElements.map((element, i) => (
+            <li key={i}>
+              <StyledMarkdown>{element}</StyledMarkdown>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 
-	const detailElementsContained = hasDetails && (
-		<div style={{ maxWidth: "30rem", padding: ".5rem" }}>
-			{detailElements.length === 1 && (
-				<StyledMarkdown>{detailElements[0]}</StyledMarkdown>
-			)}
-			{detailElements.length > 1 && (
-				<ul>
-					{detailElements.map((element, i) => (
-						<li key={i}>
-							<StyledMarkdown>{element}</StyledMarkdown>
-						</li>
-					))}
-				</ul>
-			)}
-		</div>
-	);
+  const valueIsString = typeof valueElement === "string";
+  const valueIsNumber = typeof valueElement === "number";
+  const editButton = isEditable && <EditButton editURL={editURL} />;
 
-	const valueIsString = typeof valueElement === "string";
-	const valueIsNumber = typeof valueElement === "number";
-	const editButton = isEditable && <EditButton editURL={editURL} />;
+  const displayedValueContent = valueIsString ? (
+    <span
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: ".5rem",
+      }}
+    >
+      {valueIsNumber ? String(valueElement) : null}
+      {valueIsString ? (
+        <StyledMarkdown
+          inline
+          style={{ textDecoration: hasDetails ? "underline dotted" : "none" }}
+        >
+          {valueElement}
+        </StyledMarkdown>
+      ) : null}
+      {!valueIsNumber && !valueIsString && valueElement}
+      {editButton}
+    </span>
+  ) : (
+    <>
+      <span>{valueElement}</span> {editButton}
+    </>
+  );
 
-	const displayedValueContent = valueIsString ? (
-		<span
-			style={{
-				display: "flex",
-				flexDirection: "row",
-				alignItems: "center",
-				gap: ".5rem",
-			}}
-		>
-			{valueIsNumber ? String(valueElement) : null}
-			{valueIsString ? <StyledMarkdown
-				inline
-				style={{ textDecoration: hasDetails ? "underline dotted" : "none" }}
-			>
-				{valueElement}
-			</StyledMarkdown> : null}
-			{!valueIsNumber && !valueIsString && valueElement}
-			{editButton}
-		</span>
-	) : (
-		<>
-			<span>{valueElement}</span> {editButton}
-		</>
-	);
+  const displayedValueContentWithTooltip = hasDetails ? (
+    <Tooltip content={detailElementsContained}>{displayedValueContent}</Tooltip>
+  ) : (
+    displayedValueContent
+  );
 
-	const displayedValueContentWithTooltip = hasDetails ? (
-		<Tooltip content={detailElementsContained}>
-			{displayedValueContent}
-		</Tooltip>
-	) : (
-		displayedValueContent
-	);
+  const valueElementStyle = {
+    paddingBottom: "0.25rem",
+    cursor: hasDetails ? "help" : "auto",
+  };
+  const displayedValue = isHorizontal ? (
+    displayedValueContentWithTooltip
+  ) : (
+    <td colSpan={hasDisplayedKey ? 1 : 2} style={valueElementStyle}>
+      {displayedValueContentWithTooltip}
+    </td>
+  );
 
-	const valueElementStyle = {
-		paddingBottom: "0.25rem",
-		cursor: hasDetails ? "help" : "auto",
-	};
-	const displayedValue = isHorizontal ? (
-		displayedValueContentWithTooltip
-	) : (
-		<td colSpan={hasDisplayedKey ? 1 : 2} style={valueElementStyle}>
-			{displayedValueContentWithTooltip}
-		</td>
-	);
-
-	const ListElementTag = isHorizontal ? StyledListElement : "tbody";
-	const RowTag = isHorizontal ? "div" : "tr";
-	const CellTag = isHorizontal ? "div" : "td";
-	const content = (
-		<>
-			{displayedKey}
-			{displayedValue}
-			{!isHorizontal && <CellTag style={{ textAlign: "right" }} />}
-		</>
-	);
-	const maybeWrappedContent = isHorizontal ? (
-		<StyledTag minimal large round>
-			{content}
-		</StyledTag>
-	) : (
-		<RowTag className={tagKey}>{content}</RowTag>
-	);
-	return (
-		<ListElementTag key={tagKey} className={tagKey}>
-			{maybeWrappedContent}
-		</ListElementTag>
-	);
+  const ListElementTag = isHorizontal ? StyledListElement : "tbody";
+  const RowTag = isHorizontal ? "div" : "tr";
+  const CellTag = isHorizontal ? "div" : "td";
+  const content = (
+    <>
+      {displayedKey}
+      {displayedValue}
+      {!isHorizontal && <CellTag style={{ textAlign: "right" }} />}
+    </>
+  );
+  const maybeWrappedContent = isHorizontal ? (
+    <StyledTag minimal large round>
+      {content}
+    </StyledTag>
+  ) : (
+    <RowTag className={tagKey}>{content}</RowTag>
+  );
+  return (
+    <ListElementTag key={tagKey} className={tagKey}>
+      {maybeWrappedContent}
+    </ListElementTag>
+  );
 }
