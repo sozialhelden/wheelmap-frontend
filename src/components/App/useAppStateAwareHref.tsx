97a28e2c2cedc1a27aaaa9d545f47e0ef6ea993e
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-12-17 22:11:19 +0100

    Fix a wrongly import()ed type

--- src/components/App/useAppStateAwareHref.tsx
+++ src/components/App/useAppStateAwareHref.tsx
@@ -1,8 +1,14 @@
 import { useMemo } from "react";
-import { useAppStateAwareRouter, preserveSearchParams } from "../../lib/util/useAppStateAwareRouter";
+import {
+  useAppStateAwareRouter,
+  preserveSearchParams,
+} from "../../lib/util/useAppStateAwareRouter";
 
-export function useAppStateAwareHref(href: string | import("url").UrlObject) {
+export function useAppStateAwareHref(href: string | UrlObject) {
   const { searchParams, query } = useAppStateAwareRouter();
-  const extendedHref = useMemo(() => preserveSearchParams(href, searchParams, query), [href, searchParams, query]);
+  const extendedHref = useMemo(
+    () => preserveSearchParams(href, searchParams, query),
+    [href, searchParams, query],
+  );
   return extendedHref;
 }
