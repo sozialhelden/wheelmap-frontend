1f6b6b31fdfa28dd83bf869a814e8fbb8302c5d0
Kris Siepert <m@kris.cool>
2024-12-18 15:40:14 +0100

    feat: add success screen

---
+++ src/components/CombinedFeaturePanel/components/ImageUpload/ImageUploadSuccess.tsx
@@ -0,0 +1,28 @@
+import { CheckIcon } from "@radix-ui/react-icons";
+import { Button, Flex, Strong, Text } from "@radix-ui/themes";
+import React, { type FC, useContext } from "react";
+import { t } from "ttag";
+import { ImageUploadContext } from "~/components/CombinedFeaturePanel/components/FeatureImageUpload";
+
+export const ImageUploadSuccess: FC = () => {
+  const { close } = useContext(ImageUploadContext);
+
+  return (
+    <>
+      <Flex direction="column" align="center" justify="center" py="9" px="4">
+        <CheckIcon color="green" width="50" height="50" />
+        <Text size="4">
+          <Strong>{t`Image uploaded successfully!`}</Strong>
+        </Text>
+        <Text align="center" mt="2">
+          {t`Thank you for your contribution. The image will be checked by our staff before it will be visible on Wheelmap. This can take a while, please be patient.`}
+        </Text>
+      </Flex>
+      <Flex justify="end">
+        <Button variant="soft" size="3" onClick={close}>
+          {t`Continue`}
+        </Button>
+      </Flex>
+    </>
+  );
+};
