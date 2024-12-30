05ef70f829daa8fac4fba70400f4aea2b6220022
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-12-30 19:36:33 +0100

    Wrap <AppStateLink /> with React.forwardRef

--- src/components/App/AppStateLink.tsx
+++ src/components/App/AppStateLink.tsx
@@ -1,11 +1,15 @@
-import Link from 'next/link'
-import type { ComponentProps } from 'react'
-import { useAppStateAwareHref } from './useAppStateAwareHref'
+import Link from "next/link";
+import type { ComponentProps, Ref } from "react";
+import { useAppStateAwareHref } from "./useAppStateAwareHref";
+import React from "react";
 
+export const AppStateLink = React.forwardRef(
+  (
+    { href, ...props }: ComponentProps<typeof Link>,
+    ref: Ref<HTMLAnchorElement>,
+  ) => {
+    const extendedHref = useAppStateAwareHref(href);
 
-export const AppStateLink = ({ href, ...props }: ComponentProps<typeof Link>) => {
-  const extendedHref = useAppStateAwareHref(href)
-  return <Link {...props} href={extendedHref} />
-}
-
-
+    return <Link {...props} href={extendedHref} ref={ref} />;
+  },
+);
