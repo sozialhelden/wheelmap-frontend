c6e4245f8583595ca53265d8bca616e7441110b1
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2024-12-30 19:37:15 +0100

    Use <a> tag in <JoinedEventLink /> correctly

--- src/components/App/MainMenu/link-types/JoinedEventLink.tsx
+++ src/components/App/MainMenu/link-types/JoinedEventLink.tsx
@@ -1,11 +1,8 @@
-import { useRouter } from "next/router";
-import { useCallback } from "react";
 import { useCurrentMappingEvent } from "../../../../lib/context/useCurrentMappingEvent";
 import Spinner from "../../../ActivityIndicator/Spinner";
-import { useAppStateAwareHref } from "../../useAppStateAwareHref";
 import type { IAutoLinkProps } from "./AutoLink";
 import MenuItemOrButton from "./MenuItemOrButton";
-import { Text } from "@radix-ui/themes";
+import { AppStateLink } from "../../AppStateLink";
 
 /**
  * Links to the current mapping event, or the events overview if no event is joined.
@@ -18,20 +15,13 @@ function JoinedEventLink(props: IAutoLinkProps) {
     : "/events";
 
   const label = joinedMappingEvent ? joinedMappingEvent.name : props.label;
-  const children = isValidating ? <Spinner /> : <Text>{label}</Text>;
-  const router = useRouter();
-  // XXX: This should be an <AppStateAwareLink>, but this would break keyboard navigation:
-  // A bug in Radix UI (?) appears to not update the internal `ref` correctly.
-  const hrefWithParams = useAppStateAwareHref(href);
-  const openHref = useCallback(() => {
-    router.push(hrefWithParams);
-  }, [router, hrefWithParams]);
-
-  return (
-    <MenuItemOrButton {...props} onClick={openHref}>
-      {children}
-    </MenuItemOrButton>
+  const children = isValidating ? (
+    <Spinner />
+  ) : (
+    <AppStateLink href={href}>{label}</AppStateLink>
   );
+
+  return <MenuItemOrButton {...props}>{children}</MenuItemOrButton>;
 }
 
 export default JoinedEventLink;
