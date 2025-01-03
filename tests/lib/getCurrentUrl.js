67e88bdaf2dadbb47b91d6217a21893a82665a25
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-03 20:04:07 +0100

    Remove previous test frameworks

--- tests/lib/getCurrentUrl.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function getCurrentUrl() {
-  const url = await browser.execute(() => {
-    return window.location.href;
-  });
-  console.log('Current URL:', url);
-  return url;
-};
