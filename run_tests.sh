cbe1eab76d0c73f15b5ae14018436df8d6dad76e
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-06 13:35:53 +0100

    Fix a broken shell command in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 # This is run on CI deployments.
 
-echo Running tests against URL '$CI_TEST_DEPLOYMENT_BASE_URL' (from environment variable CI_TEST_DEPLOYMENT_BASE_URL)...
+echo "Running tests against URL '$CI_TEST_DEPLOYMENT_BASE_URL' from environment variable CI_TEST_DEPLOYMENT_BASE_URL..."
 npx playwright install --with-deps
 npm install @axe-core/playwright
 npm run test -- --reporter=html
