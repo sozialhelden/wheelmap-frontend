a5bf2e8ef90821e2d361dfed52b99b1b4a06abe4
Kris Siepert <m@kris.cool>
2025-01-06 14:47:06 +0100

    feat: add wrapper-component for next.js image

--- src/components/CombinedFeaturePanel/components/Gallery/GalleryGridItem.tsx
+++ src/components/CombinedFeaturePanel/components/Gallery/GalleryGridItem.tsx
@@ -3,12 +3,11 @@ import { type FC, type MouseEventHandler, useContext, useMemo } from "react";
 import styled from "styled-components";
 import { t } from "ttag";
 import { AppStateLink } from "~/components/App/AppStateLink";
-import useAccessibilityCloudAPI from "~/lib/fetchers/ac/useAccessibilityCloudAPI";
+import Image from "~/components/Image";
 import type { AccessibilityCloudImage } from "~/lib/model/ac/Feature";
 import { GalleryContext } from "./Gallery";
-import { makeSrcSet, makeSrcSetLocation, thumbnailSizes } from "./util";
 
-const Image = styled.img`
+const StyledImage = styled(Image)`
   display: block;
   width: 100%;
   height: 100%;
@@ -18,15 +17,7 @@ const Image = styled.img`
 export const GalleryGridItem: FC<{
   image: AccessibilityCloudImage;
 }> = ({ image }) => {
-  const { baseUrl } = useAccessibilityCloudAPI({ cached: true });
   const api = useContext(GalleryContext);
-
-  const srcSet = useMemo(() => {
-    if (!baseUrl) {
-      return undefined;
-    }
-    return makeSrcSetLocation(makeSrcSet(baseUrl, thumbnailSizes, image));
-  }, [baseUrl, image]);
 
   // biome-ignore lint/correctness/useExhaustiveDependencies:
   const href = useMemo(() => api.getDetailPageUrl(image._id), [image]);
@@ -47,7 +38,7 @@ export const GalleryGridItem: FC<{
           <VisuallyHidden>{t`Open image`}</VisuallyHidden>
           <Inset asChild aria-hidden>
             <AspectRatio ratio={2 / 3}>
-              <Image className="gallery__image" srcSet={srcSet} alt="" />
+              <StyledImage image={image} height={210} alt="" />
             </AspectRatio>
           </Inset>
         </AppStateLink>
