9a60eb5d065f2e023ee4fde1fc1aa65681a04411
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-06 16:13:45 +0100

    Generate correct `/tmp/test-result-url.txt` in `run_tests.sh`

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,8 @@ echo "Running tests against URL '$CI_TEST_DEPLOYMENT_BASE_URL' from environment 
 npx playwright install --with-deps
 npm install @axe-core/playwright
 
-echo "(TODO: Generate correct URL in run_tests.sh)" > /tmp/test-result-url.txt
+# This needs to be a valid URL.
+echo $CI_TEST_DEPLOYMENT_BASE_URL > /tmp/test-result-url.txt
 
 # If tests fail, the script will exit with a non-zero status code, which will cause the CI pipeline to fail.
 PLAYWRIGHT_HTML_OUTPUT_DIR=$CI_ARTIFACTS_PATH npm run test -- --reporter=html
