0247403924ab3bd248d015e897c3ac5274567d58
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-06 14:01:46 +0100

    Change how PlayWright receives its results artifact dir

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,8 @@
 echo "Running tests against URL '$CI_TEST_DEPLOYMENT_BASE_URL' from environment variable CI_TEST_DEPLOYMENT_BASE_URL..."
 npx playwright install --with-deps
 npm install @axe-core/playwright
-npm run test -- --reporter=html
-cp -R playwright-report $CI_ARTIFACTS_PATH
+
+echo "(TODO: Generate correct URL in run_tests.sh)" > /tmp/test-result-url.txt
+
+# If tests fail, the script will exit with a non-zero status code, which will cause the CI pipeline to fail.
+PLAYWRIGHT_HTML_OUTPUT_DIR=$CI_ARTIFACTS_PATH npm run test -- --reporter=html
