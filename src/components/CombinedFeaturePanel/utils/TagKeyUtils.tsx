30412f923feff48547241d3b1523dde1e5b129fb
Mayaryin <lynn.janzen@posteo.de>
2025-01-07 12:53:39 +0100

    added a util function to strip off any present language tags. this is to avoid chaining multiple language tags accidentally

--- src/components/CombinedFeaturePanel/utils/TagKeyUtils.tsx
+++ src/components/CombinedFeaturePanel/utils/TagKeyUtils.tsx
@@ -3,7 +3,6 @@ import { languageTaggedKeys } from "~/lib/model/osm/tag-config/languageTaggedKey
 export const removeLanguageTagsIfPresent = (inputString: string) => {
   const parts: string[] = inputString.split(":");
   let assembledString: string = parts.slice(0, parts.length - 1).join(":");
-  console.log(assembledString);
   for (let i = 1; i < parts.length; i++) {
     if (languageTaggedKeys.has(assembledString)) {
       return assembledString;
