61fd20ab5082734afaba05aaffce647acf56c7a6
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-08 16:18:03 +0100

    Test commit to trigger CI

--- run_tests.sh
+++ run_tests.sh
@@ -15,4 +15,3 @@ echo $CI_TEST_DEPLOYMENT_BASE_URL > /tmp/test-result-url.txt
 
 # If tests fail, the script will exit with a non-zero status code, which will cause the CI pipeline to fail.
 PLAYWRIGHT_HTML_OUTPUT_DIR=$CI_ARTIFACTS_PATH npm run test -- --reporter=html
-
