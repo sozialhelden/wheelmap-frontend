3d9392be1ec13cde3013466124902df0e9d2cb55
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-13 20:11:42 +0100

    Remove test result URL from run_tests.sh
    This means the CI process should recognize that there is an index.html, leading to the artifact folder URL being stored in the GitHub commit status.

--- run_tests.sh
+++ run_tests.sh
@@ -10,9 +10,6 @@ echo "Running tests against URL '$CI_TEST_DEPLOYMENT_BASE_URL' from environment 
 npx playwright install --with-deps
 npm install @axe-core/playwright
 
-# This needs to be a valid URL.
-echo $CI_TEST_DEPLOYMENT_BASE_URL > /tmp/test-result-url.txt
-
 # If tests fail, the script will exit with a non-zero status code, which will cause the CI pipeline to fail.
 PLAYWRIGHT_HTML_OUTPUT_DIR=$CI_ARTIFACTS_PATH npm run test -- --reporter=html --trace=retain-on-failure
 
