defd9c69829006ce6ae02e9b36eb59c5e6a86a15
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-01-13 17:13:35 +0100

    Fix onboarding skipping in CI tests

--- e2e/skipOnboarding.ts
+++ e2e/skipOnboarding.ts
@@ -2,6 +2,6 @@ import { Page } from "@playwright/test";
 
 export async function skipOnboarding(page: Page) {
   await page.getByRole('button', { name: 'Okay, let’s go!' }).click();
-  await page.getByRole('button', { name: 'Continue without location access' }).click();
+  await page.getByRole('button', { name: 'Skip' }).click();
   await page.getByRole('button', { name: 'Let’s go!' }).click();
 }
