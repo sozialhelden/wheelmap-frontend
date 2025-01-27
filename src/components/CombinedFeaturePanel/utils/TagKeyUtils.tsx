8434a3c295cd38d039994ed37517c8f52e2d2d8f
Mayaryin <lynn.janzen@posteo.de>
2025-01-27 17:46:55 +0100

    remove console logs

--- src/components/CombinedFeaturePanel/utils/TagKeyUtils.tsx
+++ src/components/CombinedFeaturePanel/utils/TagKeyUtils.tsx
@@ -7,14 +7,14 @@ export const removeLanguageTagsIfPresent = (inputString: string) => {
   }
   const parts: string[] = inputString.split(":");
   let assembledString: string = parts.slice(0, parts.length - 1).join(":");
-  console.log("start string: ", assembledString);
+
   for (let i = 1; i < parts.length; i++) {
     if (languageTaggedKeys.has(assembledString)) {
       return  { normalizedTag: assembledString, isLanguageTagged: true };
     }
     assembledString = parts.slice(0, parts.length - (1 + i)).join(":");
   }
-  console.log("Tag is not language-tagged. Returning original input string.");
+  //console.log("Tag is not language-tagged. Returning original input string.");
   return  { normalizedTag: inputString, isLanguageTagged: false };
 };
 
