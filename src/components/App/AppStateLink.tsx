b379ac928fb5bf04eeae86d62283068e69083f89
Kris Siepert <m@kris.cool>
2025-02-04 16:30:12 +0100

    refactor: simplify app state link

--- src/components/App/AppStateLink.tsx
+++ src/components/App/AppStateLink.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
-import type { ComponentProps, Ref } from "react";
+import { type ComponentProps, type Ref, useMemo } from "react";
 import React from "react";
-import { useAppStateAwareHref } from "./useAppStateAwareHref";
+import {
+  preserveSearchParams,
+  useAppStateAwareRouter,
+} from "~/lib/util/useAppStateAwareRouter";
 
 export const AppStateLink = React.forwardRef(
   (
     { href, ...props }: ComponentProps<typeof Link>,
     ref: Ref<HTMLAnchorElement>,
   ) => {
-    const extendedHref = useAppStateAwareHref(href);
+    const { searchParams, query } = useAppStateAwareRouter();
+
+    const extendedHref = useMemo(
+      () => preserveSearchParams(href, searchParams, query),
+      [href, searchParams, query],
+    );
 
     return <Link {...props} href={extendedHref} ref={ref} />;
   },
