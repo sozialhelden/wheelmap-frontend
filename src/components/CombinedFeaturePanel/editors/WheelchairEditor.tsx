3abe1d1d60d1de10e0dbd099b8477e6bdba297ae
Mayaryin <lynn.janzen@posteo.de>
2025-02-06 15:01:28 +0100

    wrap in radix dialog, swap header with dialog description, add aria labels, translate texts

--- src/components/CombinedFeaturePanel/editors/WheelchairEditor.tsx
+++ src/components/CombinedFeaturePanel/editors/WheelchairEditor.tsx
@@ -1,5 +1,7 @@
-import { Button, Link } from "@radix-ui/themes";
-import React, { useContext, useEffect, useState } from "react";
+import { Button, Dialog, Flex } from "@radix-ui/themes";
+import type React from "react";
+import { useContext, useEffect, useState } from "react";
+import { t } from "ttag";
 import { useCurrentLanguageTagStrings } from "~/lib/context/LanguageTagContext";
 import type { YesNoLimitedUnknown } from "~/lib/model/ac/Feature";
 import { unknownCategory } from "~/lib/model/ac/categories/Categories";
@@ -14,7 +16,7 @@ import FeatureImage from "../components/image/FeatureImage";
 import { useFeatureLabel } from "../utils/useFeatureLabel";
 import type { BaseEditorProps } from "./BaseEditor";
 
-export const WheelchairEditor: React.FC<BaseEditorProps> =({
+export const WheelchairEditor: React.FC<BaseEditorProps> = ({
   feature,
   onChange,
   handleSubmitButtonClick,
@@ -44,73 +46,93 @@ export const WheelchairEditor: React.FC<BaseEditorProps> =({
   }, [current, editedTagValue]);
 
   return (
-    <StyledReportView className="_view">
-      <FeatureNameHeader feature={feature}>
-        {feature["@type"] === "osm:Feature" && (
-          <FeatureImage feature={feature} />
-        )}
-      </FeatureNameHeader>
-      <h2 className="_title">How wheelchair accessible is this place?</h2>
-      <form>
-        <AccessibilityView
-          onClick={() => {
-            setEditedTagValue("yes");
-            onChange?.("yes");
-          }}
-          className="_yes"
-          inputLabel="accessibility-fully"
-          selected={editedTagValue === "yes"}
-          icon={<Icon size="medium" accessibility="yes" category={cat} />}
-          valueName="Fully"
-        >
-          Entrance has no steps, and all rooms are accessible without steps.
-        </AccessibilityView>
-        <AccessibilityView
-          onClick={() => {
-            setEditedTagValue("limited");
-            onChange?.("limited");
-          }}
-          className="_okay"
-          inputLabel="accessibility-partially"
-          selected={editedTagValue === "limited"}
-          icon={<Icon size="medium" accessibility="limited" category={cat} />}
-          valueName="Partially"
-        >
-          Entrance has one step with max. 3 inches height, most rooms are
-          without steps
-        </AccessibilityView>
+    <Dialog.Root open>
+      <Dialog.Content
+        aria-label={t`Toilet Accessibility Editor`}
+        aria-describedby="dialog-description"
+      >
+        <Flex direction="column" gap="4" style={{ padding: "10px" }}>
+          <StyledReportView className="_view">
+            <FeatureNameHeader feature={feature}>
+              {feature["@type"] === "osm:Feature" && (
+                <FeatureImage feature={feature} />
+              )}
+            </FeatureNameHeader>
 
-        <AccessibilityView
-          onClick={() => {
-            setEditedTagValue("no");
-            onChange?.("no");
-          }}
-          className="_no"
-          inputLabel="accessibility-not-at-all"
-          selected={editedTagValue === "no"}
-          icon={<Icon size="medium" accessibility="no" category={cat} />}
-          valueName="Not at all"
-        >
-          Entrance has a high step or several steps, none of the rooms are
-          accessible.
-        </AccessibilityView>
-      </form>
+            <Dialog.Description id="dialog-description" size="3">
+              {t`How wheelchair accessible is this place?`}
+            </Dialog.Description>
 
-      <footer className="_footer">
-        <AppStateLink href={baseFeatureUrl} tabIndex={-1}>
-          <Link href="">{saveButtonDoesNothing ? "Cancel" : "Back"}</Link>
-        </AppStateLink>
-        <AppStateLink href={baseFeatureUrl} tabIndex={-1}>
-          <Button
-            variant="solid"
-            onClick={
-              saveButtonDoesNothing ? undefined : handleSubmitButtonClick
-            }
-          >
-            {saveButtonDoesNothing ? "Confirm" : "Send"}
-          </Button>
-        </AppStateLink>
-      </footer>
-    </StyledReportView>
+            <form>
+              <AccessibilityView
+                onClick={() => {
+                  setEditedTagValue("yes");
+                  onChange?.("yes");
+                }}
+                className="_yes"
+                inputLabel="accessibility-fully"
+                selected={editedTagValue === "yes"}
+                icon={<Icon size="medium" accessibility="yes" category={cat} />}
+                valueName="Fully"
+              >
+                {t`Entrance has no steps, and all rooms are accessible without steps.`}
+              </AccessibilityView>
+              <AccessibilityView
+                onClick={() => {
+                  setEditedTagValue("limited");
+                  onChange?.("limited");
+                }}
+                className="_okay"
+                inputLabel="accessibility-partially"
+                selected={editedTagValue === "limited"}
+                icon={
+                  <Icon size="medium" accessibility="limited" category={cat} />
+                }
+                valueName="Partially"
+              >
+                {t`Entrance has one step with max. 3 inches height, most rooms are
+                without steps`}
+              </AccessibilityView>
+
+              <AccessibilityView
+                onClick={() => {
+                  setEditedTagValue("no");
+                  onChange?.("no");
+                }}
+                className="_no"
+                inputLabel="accessibility-not-at-all"
+                selected={editedTagValue === "no"}
+                icon={<Icon size="medium" accessibility="no" category={cat} />}
+                valueName="Not at all"
+              >
+                {t`Entrance has a high step or several steps, none of the rooms are
+                accessible.`}
+              </AccessibilityView>
+            </form>
+
+            <Flex gap="3" mt="3" justify="end">
+              <AppStateLink href={baseFeatureUrl} tabIndex={-1}>
+                <Button variant="soft" size="2" aria-label={t`Cancel`}>
+                  {t`Cancel`}
+                </Button>
+              </AppStateLink>
+
+              <AppStateLink href={baseFeatureUrl} tabIndex={-1}>
+                <Button
+                  variant="solid"
+                  size="2"
+                  aria-label={saveButtonDoesNothing ? t`Confirm` : t`Send`}
+                  onClick={
+                    saveButtonDoesNothing ? undefined : handleSubmitButtonClick
+                  }
+                >
+                  {saveButtonDoesNothing ? t`Confirm` : t`Send`}
+                </Button>
+              </AppStateLink>
+            </Flex>
+          </StyledReportView>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
