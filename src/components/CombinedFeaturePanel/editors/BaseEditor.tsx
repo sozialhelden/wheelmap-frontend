1cde3994f516af0cd888faaf5f62b20f18212749
Mayaryin <lynn.janzen@posteo.de>
2025-02-06 11:35:18 +0100

    rename handleSubmitButtonClick to onSubmit

--- src/components/CombinedFeaturePanel/editors/BaseEditor.tsx
+++ src/components/CombinedFeaturePanel/editors/BaseEditor.tsx
@@ -1,13 +1,13 @@
 import type React from "react";
-import type {AnyFeature} from "~/lib/model/geo/AnyFeature";
-import type {EditorTagValue} from "./EditorTagValue";
+import type { AnyFeature } from "~/lib/model/geo/AnyFeature";
+import type { EditorTagValue } from "./EditorTagValue";
 
 export type BaseEditorProps = {
   feature: AnyFeature;
   tagKey?: string;
   onUrlMutationSuccess?: (urls: string[]) => void;
   onChange?: (tagValue: EditorTagValue) => void;
-  handleSubmitButtonClick?: () => void;
+  onSubmit?: () => void;
   addingNewLanguage?: boolean;
   onLanguageChange?: (languageTag: string) => void;
 };
