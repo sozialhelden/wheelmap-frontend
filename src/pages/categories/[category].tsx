2180958605a4d6aebb10937c50156be034b947a2
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2025-02-07 11:26:54 +0100

    Organize imports in pages, fix a build error

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -1,9 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
 import { getLayout } from "../../components/App/MapLayout";
-import useCategory from "../../lib/fetchers/ac/refactor-this/useCategory";
-import MockedPOIDetails from "../../lib/fixtures/mocks/features/MockedPOIDetails";
-import { anyFeature } from "../../lib/fixtures/mocks/features/anyfeature";
 
 function CategoryPage() {
   const router = useRouter();
