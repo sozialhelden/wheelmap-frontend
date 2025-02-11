8c31bd781de0636830d015eac8f33251d171180b
Mayaryin <lynn.janzen@posteo.de>
2025-02-11 13:54:16 +0100

    show the right dialog description and the language picker when no description is available at all

--- src/components/CombinedFeaturePanel/components/AccessibilitySection/AddWheelchairDescription.tsx
+++ src/components/CombinedFeaturePanel/components/AccessibilitySection/AddWheelchairDescription.tsx
@@ -1,17 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import {
   normalizeLanguageTag,
   useCurrentLanguageTagStrings,
 } from "../../../../lib/context/LanguageTagContext";
-import { FeaturePanelContext } from "../../FeaturePanelContext";
 import { EditButton } from "./EditButton";
 
 export default function AddWheelchairDescription() {
-  const { baseFeatureUrl } = useContext(FeaturePanelContext);
   const languageTags = useCurrentLanguageTagStrings();
   const normalizedLanguageTag = normalizeLanguageTag(languageTags[0]);
   const key = `wheelchair:description:${normalizedLanguageTag}`;
-  const editURL = `${baseFeatureUrl}/edit/${key}`;
-  const editButton = <EditButton editURL={editURL} />;
+  const editButton = <EditButton tagKey={key} addingNewLanguage={true} />;
   return editButton;
 }
