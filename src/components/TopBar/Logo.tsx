14565c1ebcdaf55355fc4c4db0ecbcda65be6ff5
Kris Siepert <m@kris.cool>
2025-02-12 16:56:57 +0100

    fix: logo

--- src/components/TopBar/Logo.tsx
+++ src/components/TopBar/Logo.tsx
@@ -1,24 +1,28 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
+import styled from "styled-components";
 import { t } from "ttag";
 import type { IBranding } from "~/lib/model/ac/IBranding";
 import VectorImage from "../shared/VectorImage";
+
+const StyledButton = styled(Button)`
+    max-width: min(100%, 150px);
+`;
 
 export default function Logo({
   branding,
   productName,
 }: { branding?: IBranding; productName: string }) {
   return (
-    <Button aria-label={t`Home`} variant="ghost" radius="none" asChild>
+    <StyledButton aria-label={t`Home`} variant="ghost" radius="none" asChild>
       <Link href="/onboarding">
         <VectorImage
           svg={branding?.vectorLogoSVG}
-          maxHeight="30px"
-          maxWidth="150px"
+          maxWidth="100%"
           hasShadow={false}
           svgHTMLAttributes={{ "aria-label": t`${productName} logo` }}
         />
       </Link>
-    </Button>
+    </StyledButton>
   );
 }
