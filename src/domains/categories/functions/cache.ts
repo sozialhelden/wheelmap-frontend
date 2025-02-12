d88b9d90228c56536eb07a4895afb65edc7df1ca
Kris Siepert <m@kris.cool>
2025-02-12 16:16:16 +0100

    chore: add biome exceptions

--- src/domains/categories/functions/cache.ts
+++ src/domains/categories/functions/cache.ts
@@ -1,75 +1,77 @@
-import type {ACCategory} from "~/domains/categories/types/ACCategory";
-import type {AnyFeature} from "~/lib/model/geo/AnyFeature";
+import type { ACCategory } from "~/domains/categories/types/ACCategory";
+import type { AnyFeature } from "~/lib/model/geo/AnyFeature";
 
 type SynonymCache = Map<string, ACCategory>;
 export const unknownCategory: Readonly<ACCategory> = {
-    _id: "unknown",
-    translations: {
-        _id: "Unknown",
-    },
-    icon: "unknown",
-    synonyms: [],
-    parentIds: [],
+  _id: "unknown",
+  translations: {
+    _id: "Unknown",
+  },
+  icon: "unknown",
+  synonyms: [],
+  parentIds: [],
 };
 export type CategoryLookupTables = {
-    synonymCache: SynonymCache | undefined;
-    categories: ACCategory[];
+  synonymCache: SynonymCache | undefined;
+  categories: ACCategory[];
 };
 
 export function getCategory(
-    synonymCache: SynonymCache,
-    idOrSynonym: string | number,
+  synonymCache: SynonymCache,
+  idOrSynonym: string | number,
 ): ACCategory | undefined {
-    return synonymCache.get(String(idOrSynonym));
+  return synonymCache.get(String(idOrSynonym));
 }
 
 export function generateSynonymCache(categories: ACCategory[]): SynonymCache {
-    const result: SynonymCache = new Map();
-    categories.forEach((category) => {
-        result.set(category._id, category);
-        const {synonyms} = category;
-        if (!Array.isArray(synonyms)) {
-            return;
-        }
-        synonyms.forEach((synonym) => {
-            result.set(synonym, category);
-        });
+  const result: SynonymCache = new Map();
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  categories.forEach((category) => {
+    result.set(category._id, category);
+    const { synonyms } = category;
+    if (!Array.isArray(synonyms)) {
+      return;
+    }
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    synonyms.forEach((synonym) => {
+      result.set(synonym, category);
     });
-    return result;
+  });
+  return result;
 }
 
 export function getCategoryForFeature(
-    synonymCache: SynonymCache,
-    feature: AnyFeature,
+  synonymCache: SynonymCache,
+  feature: AnyFeature,
 ): ACCategory | undefined {
-    const {properties} = feature;
-    if (!properties) {
-        return unknownCategory;
-    }
+  const { properties } = feature;
+  if (!properties) {
+    return unknownCategory;
+  }
 
-    let categoryId: string | undefined;
-    if (
-        feature["@type"] === "a11yjson:PlaceInfo" ||
-        feature["@type"] === "a11yjson:EquipmentInfo" ||
-        feature["@type"] === "ac:PlaceInfo" ||
-        feature["@type"] === "ac:EquipmentInfo"
-    ) {
-        categoryId = feature.properties?.category;
-    } else if (feature["@type"] === "photon:SearchResult") {
-        categoryId = feature.properties.osm_value || feature.properties.osm_key;
-    } else if (feature["@type"] === "osm:Feature") {
-        for (const [key, value] of Object.entries(feature.properties)) {
-            const foundCategory = getCategory(synonymCache, `${key}=${value}`);
-            if (foundCategory) {
-                return foundCategory;
-            }
-        }
-        categoryId = feature.properties.amenity as string | undefined;
+  let categoryId: string | undefined;
+  if (
+    feature["@type"] === "a11yjson:PlaceInfo" ||
+    feature["@type"] === "a11yjson:EquipmentInfo" ||
+    feature["@type"] === "ac:PlaceInfo" ||
+    feature["@type"] === "ac:EquipmentInfo"
+  ) {
+    categoryId = feature.properties?.category;
+  } else if (feature["@type"] === "photon:SearchResult") {
+    categoryId = feature.properties.osm_value || feature.properties.osm_key;
+  } else if (feature["@type"] === "osm:Feature") {
+    for (const [key, value] of Object.entries(feature.properties)) {
+      const foundCategory = getCategory(synonymCache, `${key}=${value}`);
+      if (foundCategory) {
+        return foundCategory;
+      }
     }
+    categoryId = feature.properties.amenity as string | undefined;
+  }
 
-    if (!categoryId) {
-        return unknownCategory;
-    }
+  if (!categoryId) {
+    return unknownCategory;
+  }
 
-    return getCategory(synonymCache, String(categoryId));
+  return getCategory(synonymCache, String(categoryId));
 }
