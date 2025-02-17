1a9553c38e66c6d88c8e74b6340f9dea75628f99
Mayaryin <lynn.janzen@posteo.de>
2025-02-17 22:07:42 +0100

    Fix exessive toasts: added auto-close and shorter delay to toasts for slow loading

--- src/lib/fetchers/SWRConfigProvider.tsx
+++ src/lib/fetchers/SWRConfigProvider.tsx
@@ -2,27 +2,12 @@ import { t } from "@transifex/native";
 import * as React from "react";
 import { toast } from "react-toastify";
 import { SWRConfig, type SWRConfiguration } from "swr";
-import { ErrorMessage } from "~/components/SWRError/ErrorMessage";
 import type ResourceError from "~/lib/fetchers/ResourceError";
 
 const globalSWRConfig: SWRConfiguration<unknown, ResourceError> = {
   onError: (error, key) => {
-    const toastId = key;
     if (error) {
       console.error("SWR error", error);
-      const errorElement = <ErrorMessage error={error} />;
-      toast.error(errorElement, {
-        toastId,
-        delay: 2000,
-        autoClose: false,
-        position: "bottom-right",
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
     }
   },
   onLoadingSlow(key, config) {
@@ -30,8 +15,8 @@ const globalSWRConfig: SWRConfiguration<unknown, ResourceError> = {
       t("Loading seems to take a bit longer than usual. Please hold the line…"),
       {
         toastId: key,
-        delay: 2000,
-        autoClose: false,
+        delay: 1500,
+        autoClose: 5000,
         position: "bottom-right",
         hideProgressBar: false,
         closeOnClick: false,
