8409fdade7bd52a635b8a5b56fa29ff565a2b4a5
Kris Siepert <m@kris.cool>
2025-02-20 13:39:50 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/domains/list/ListItem.tsx

--- src/domains/list/ListItem.tsx
+++ src/domains/list/ListItem.tsx
@@ -1,16 +1,41 @@
-type Props = {
-  name: string;
-  category: string;
-  image?: string;
-};
+import styled from "styled-components";
+import WikidataEntityImage from "~/components/CombinedFeaturePanel/components/image/WikidataEntityImage";
+import { useFeatureLabel } from "~/components/CombinedFeaturePanel/utils/useFeatureLabel";
+import { useCurrentLanguageTagStrings } from "~/lib/context/LanguageTagContext";
+import type { AnyFeature } from "~/lib/model/geo/AnyFeature";
 
-export function ListItem(props: Props) {
-  const { name, category, image } = props;
+const Container = styled.div`
+    display: flex;
+`;
+
+export function ListItem({ feature }: { feature: AnyFeature }) {
+  const languageTags = useCurrentLanguageTagStrings();
+
+  // ariaLabel: "Siegessäule, Viewpoint"
+  // buildingName: undefined
+  // buildingNumber: undefined
+  // category: Object { _id: "viewpoint", icon: "viewpoint", parentIds: (1) […], … }
+  // categoryName: "Viewpoint"
+  // categoryTagKeys: Array []
+  // hasLongName: false
+  // levelName: undefined
+  // localRef: undefined
+  // parentPlaceName: undefined
+  // placeName: "Siegessäule"
+  // ref: undefined
+  // roomNameAndNumber: " "
+  const { placeName, categoryName } = useFeatureLabel({
+    feature,
+    languageTags,
+  });
+
   return (
-    <div>
-      {name}
-      {category}
-      {image}
-    </div>
+    <Container>
+      <div>
+        <p>{placeName}</p>
+        <p>{categoryName}</p>
+      </div>
+      <WikidataEntityImage feature={feature} />
+    </Container>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { getLayout } from "~/components/App/MapLayout";
+import { useFeatureLabel } from "~/components/CombinedFeaturePanel/utils/useFeatureLabel";
 import { CategoryFilter } from "~/domains/categories/components/CategoryFilter";
+import useCategory from "~/domains/categories/hooks/useCategory";
+import { ListItem } from "~/domains/list/ListItem";
 import { Search } from "~/domains/search/components/Search";
 import { useSheetContext } from "~/domains/sheet/SheetContext.tsx";
 import { Sheet } from "~/domains/sheet/components/Sheet";
+import { useCurrentLanguageTagStrings } from "~/lib/context/LanguageTagContext";
 import { isFirstStart } from "~/lib/util/savedState";
 
 const SearchToolbar = styled.div`
@@ -48,6 +52,28 @@ export default function Page() {
     }
   }, []);
 
+  const testFeature = {
+    "@type": "osm:Feature",
+    type: "Feature",
+    _id: "node/1097191894",
+    geometry: { type: "Point", coordinates: [13.350111, 52.514507] },
+    centroid: { type: "Point", coordinates: [13.350111, 52.514507] },
+    properties: {
+      fee: "yes",
+      icon: "viewpoint",
+      name: "Siegessäule",
+      image: "https://photos.app.goo.gl/5NQPGfqqGtigyk517",
+      charge: "4 EUR",
+      height: 58.6,
+      noname: "yes",
+      tourism: "viewpoint",
+      direction: "0-360",
+      wheelchair: "no",
+      opening_hours:
+        "Nov-Mar: Mo-Fr 10:00-17:00; Nov-Mar: Sa,Su 10:00-17:30; Apr-Oct: Mo-Fr 09:30-18:30; Apr-Oct: Sa,Su 09:30-19:00",
+    },
+  };
+
   return (
     <>
       <SearchToolbar>
@@ -70,14 +96,7 @@ export default function Page() {
         <CategoryFilter />
       </SearchToolbar>
       <Sheet scrollStops={[0.5]}>
-        {[...Array(10).keys()].map((i) => (
-          <Flex key={i} style={{ margin: "0 0 1rem 0" }}>
-            <img
-              src="https://shop.joinmastodon.org/cdn/shop/files/IMG_0145.jpg?v=1726292615&width=1100"
-              style={{ maxWidth: "100%" }}
-            />
-          </Flex>
-        ))}
+        <ListItem feature={testFeature} />
       </Sheet>
     </>
   );
