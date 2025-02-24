a2d815025658fadd3ec3157b4ff215a05d736f97
Mayaryin <lynn.janzen@posteo.de>
2025-02-24 18:26:35 +0100

    apply discussed changes: - forward ref - using radix themes button props - spreading operator - iconOnly flag - children

--- src/components/shared/Buttons.tsx
+++ src/components/shared/Buttons.tsx
@@ -1,59 +1,53 @@
-import { Button } from "@radix-ui/themes";
-import type { Responsive } from "@radix-ui/themes/dist/cjs/props";
-import type React from "react";
+import { Button, type ButtonProps, IconButton } from "@radix-ui/themes";
+import type { ReactNode } from "react";
+import { forwardRef } from "react";
 
-type Props = {
-  text: string;
-  onClick: () => void;
-  size?: Responsive<"3" | "1" | "2" | "4">;
-  disabled?: boolean;
-  loading?: boolean;
-};
+type Props = { children: ReactNode; iconOnly?: boolean } & ButtonProps;
 
-export const PrimaryButton = ({
-  text,
-  onClick,
-  size = "3",
-  disabled = false,
-  loading = false,
-}: Props) => {
-  return (
-    <Button
-      variant="solid"
-      size={size}
-      onClick={onClick}
-      disabled={disabled}
-      loading={loading}
-    >
-      {text}
-    </Button>
-  );
-};
+export const PrimaryButton = forwardRef<HTMLButtonElement, Props>(
+  function PrimaryButton(
+    { children, size = "3", iconOnly = false, ...props },
+    ref,
+  ) {
+    const Component = iconOnly ? IconButton : Button;
+    return (
+      <Component ref={ref} variant="solid" size={size} {...props}>
+        {children}
+      </Component>
+    );
+  },
+);
 
-export const SecondaryButton = ({ text, onClick, size = "3" }: Props) => {
-  return (
-    <Button
-      color="gray"
-      variant="soft"
-      size={size}
-      highContrast
-      onClick={onClick}
-    >
-      {text}
-    </Button>
-  );
-};
+export const SecondaryButton = forwardRef<HTMLButtonElement, Props>(
+  function SecondaryButton(
+    { children, size = "3", iconOnly = false, ...props },
+    ref,
+  ) {
+    const Component = iconOnly ? IconButton : Button;
+    return (
+      <Component ref={ref} variant="soft" size={size} highContrast {...props}>
+        {children}
+      </Component>
+    );
+  },
+);
 
-export const TertiaryButton = ({ text, onClick, size = "3" }: Props) => {
-  return (
-    <Button
-      color="gray"
-      variant="surface"
-      size={size}
-      highContrast
-      onClick={onClick}
-    >
-      {text}
-    </Button>
-  );
-};
+export const TertiaryButton = forwardRef<HTMLButtonElement, Props>(
+  function TertiaryButton(
+    { children, size = "3", iconOnly = false, ...props },
+    ref,
+  ) {
+    const Component = iconOnly ? IconButton : Button;
+    return (
+      <Component
+        ref={ref}
+        variant="outline"
+        size={size}
+        highContrast
+        {...props}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
