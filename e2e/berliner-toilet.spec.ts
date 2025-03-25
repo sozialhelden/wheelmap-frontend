a88f38713139cef153728a1fd37e42ff6c8ba461
Nina Eilers <n.eilers-koenig@gmx.net>
2025-03-25 14:07:26 +0100

    first commit on ER University building test and on berliner-toilet test

---
+++ e2e/berliner-toilet.spec.ts
@@ -0,0 +1,70 @@
+import useSubmitNewValueCallback from '~/lib/fetchers/osm-api/makeChangeRequestToOsmApi';
+import { test, expect } from './lib/axe-test';
+import getBaseURL from './lib/base-url';
+import { skipOnDesktops, skipOnMobiles } from './lib/device-type';
+import { skipOnboarding } from './skipOnboarding';
+
+const baseURL = getBaseURL();
+
+test.beforeEach(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto(baseURL);
+  await skipOnboarding(page);
+});
+
+test('Goto Open on OpenStreetMap', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/composite/ac:PlaceInfo:WcdW6a8tJumrqsswk,buildings:way:718035022');
+  await page.waitForLoadState();
+  //goto Open Street Map
+  await page.goto('https://www.openstreetmap.org/#map=19/52.514553/13.350202');
+  await page.waitForLoadState();
+  skipOnMobiles();
+  //goto Siegessäule
+  await page.getByRole('textbox', { name: 'Search' }).fill('Siegessäule');
+  await page.waitForLoadState();
+  await expect(page).toHaveTitle("OpenStreetMap");
+  await page.goto('https://www.openstreetmap.org/search?query=Siegessäule#map=19/52.514513/13.350111');
+  await page.getByRole('button', { name: 'Go' }).click();
+  await page.waitForLoadState();
+  await expect(page.getByText('Victory Column')).toBeVisible();
+});
+ 
+//test('Goto Open on Bing Maps', async ({ page }) => {//
+  test('Goto Open on Apple Maps', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/composite/ac:PlaceInfo:WcdW6a8tJumrqsswk,buildings:way:718035022');
+  await page.waitForLoadState();
+  skipOnMobiles();
+  //goto Siegessäule
+  await page.goto('http://maps.apple.com/?ll=52.5145533,13.3502022&q=Gro%C3%9Fer%20Stern');
+  await page.waitForLoadState();  
+  await expect(page.getByRole('heading', { name: 'Großer Stern' })).toBeVisible();
+  await expect(page.getByText('Victory Column')).toBeVisible();
+});
+
+test('EmbassyOfSwitzerland-MyOwnAdress', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/composite/amenities:relation:2886766,buildings:relation:2886766');
+  await page.waitForLoadState();
+  await expect(page.getByText('Otto-von-Bismarck-Allee')).toBeVisible();
+  await page.goto('http://maps.apple.com/?ll=13.371418,52.521004,13.371544,52.521004,13.371544,52.521065,13.371418,52.521065,13.371418,52.521004,13.3709,52.520999,13.370902,52.521152,13.370913,52.521152,13.370928,52.521151,13.370971,52.521151,13.370972,52.521153,13.370973,52.521155,13.370974,52.521158,13.370976,52.52116,13.370978,52.521162,13.370983,52.521164,13.370987,52.521166,13.370992,52.521167,13.370999,52.521168,13.371006,52.521168,13.371012,52.521167,13.371018,52.521166,13.371022,52.521164,13.371025,52.521161,13.371409,52.52116,13.371408,52.521205,13.371527,52.521203,13.371646,52.521202,13.371662,52.521202,13.371659,52.520999,13.3709,52.520999&q=Embassy%20of%20Switzerland');
+  await page.waitForLoadState();  
+  await expect(page.getByRole('heading', { name: 'Embassy of Switzerland' })).toBeVisible();
+});
+
+test('EmbassyOfSwitzerland-OnTheWeb', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/composite/amenities:relation:2886766,buildings:relation:2886766');
+  await page.waitForLoadState();
+  await expect(page.getByText('https://www.eda.admin.ch/berlin.html')).toBeVisible();
+  await page.goto('http://www.eda.admin.ch/berlin.html');
+  await page.waitForLoadState();  
+  await expect(page.locator('text=Schweiz und Deutschland').first()).toBeVisible();
+});
+
+test('EmbassyOfSwitzerland-CallMyPhoneNumber', async ({ page }) => {
+
+  await page.goto('https://feature-a11ymap.wheelmap.tech/composite/amenities:relation:2886766,buildings:relation:2886766');
+  await page.waitForLoadState();
+  await expect(page.getByText('Call +49 30 390 40 00')).toBeVisible();
+  await page.waitForLoadState();
+  await page.locator('text=Call +49 30 390 40 00').first().click(); 
+  await page.waitForLoadState(); 
+});
---
+++ e2e/how-to-get-there.spec.ts
@@ -0,0 +1,42 @@
+import { Label } from '@blueprintjs/core';
+import { test, expect } from './lib/axe-test';
+import getBaseURL from './lib/base-url';
+import { skipOnDesktops, skipOnMobiles } from './lib/device-type';
+import { skipOnboarding } from './skipOnboarding';
+
+const baseURL = getBaseURL();
+
+test.beforeEach(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto(baseURL);
+  await skipOnboarding(page);
+});
+
+test('Goto Access ER University', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/buildings/way%3A23517902?category=&q=&wheelchair=unknown&toilet=yes&lat=52.51096600&lon=13.32576500&extent=13.3248001&extent=52.5113331&extent=13.3267297&extent=52.5105994&zoom=18.74388041');
+  await page.waitForLoadState();
+  await page.click('text="Access"');
+  await page.click('text="Partially wheelchair accessible"');
+  await page.waitForLoadState();
+  await expect(page.getByRole('button').first()).toBeVisible();
+    //<button data-accent-color="" aria-label="Edit" tabindex="-1" class="rt-reset rt-BaseButton rt-r-size-2 rt-variant-soft rt-IconButton"><svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></button>
+});
+
+test('Goto Look ER University', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/buildings/way%3A23517902?category=&q=&wheelchair=unknown&toilet=yes&lat=52.51096600&lon=13.32576500&extent=13.3248001&extent=52.5113331&extent=13.3267297&extent=52.5105994&zoom=18.74388041');
+  await page.waitForLoadState();
+  await page.click('text="Look"');
+  await page.waitForLoadState();
+  await expect(page.getByText('4 levels')).toBeVisible();
+  await page.waitForLoadState();
+});  
+
+
+test('Goto Operator ER University', async ({ page }) => {
+  await page.goto('https://feature-a11ymap.wheelmap.tech/buildings/way%3A23517902?category=&q=&wheelchair=unknown&toilet=yes&lat=52.51096600&lon=13.32576500&extent=13.3248001&extent=52.5113331&extent=13.3267297&extent=52.5105994&zoom=18.74388041');
+  await page.waitForLoadState();
+  await page.click('text="Operator"');
+  await page.click('text="Technische Universität Berlin"');
+  await page.waitForLoadState();
+  await expect(page.getByRole('button').first()).toBeVisible();
+});
