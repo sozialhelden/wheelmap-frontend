d78a819ed141a693fe9239081435a7eba68e82bf
Kris Siepert <m@kris.cool>
2025-03-27 14:24:23 +0100

    chore: add beta flag

--- src/modules/osm-api/hooks/useOsmApi.ts
+++ src/modules/osm-api/hooks/useOsmApi.ts
@@ -1,5 +1,5 @@
 import { useCurrentAppToken } from "~/lib/context/AppContext";
-import { useEnvContext } from "~/lib/context/EnvContext";
+import { useEnvironmentContext } from "~/modules/app/context/EnvironmentContext";
 
 export type OsmApiProps = {
   tileNumber?: number;
@@ -10,7 +10,7 @@ export default function useOsmApi({
   tileNumber = 1,
   cached = false,
 }: OsmApiProps = {}) {
-  const env = useEnvContext();
+  const env = useEnvironmentContext();
   const appToken = useCurrentAppToken();
 
   const baseUrl =
