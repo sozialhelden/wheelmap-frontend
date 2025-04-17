de04b3956fbf39f0ea4620134e0f1ba279195e21
Mayaryin <lynn.janzen@posteo.de>
2025-04-17 14:03:28 +0200

    temporarily remove oboarding tests and add more workers in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,9 +19,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 1,
+  retries: process.env.CI ? 1 : 0,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -48,27 +47,24 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: "mobile-chromium",
-      use: { ...devices["Pixel 5"] },
-    },
+    // {
+    //   name: "mobile-chromium",
+    //   use: { ...devices["Pixel 5"] },
+    // },
     {
       name: "mobile-safari",
       use: { ...devices["iPhone 12"] },
     },
-
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
