ac49de4fb6b0d574fc1676952d0a0c593196d6d0
Mayaryin <lynn.janzen@posteo.de>
2025-04-29 18:34:18 +0200

    build feature description component

---
+++ src/modules/feature-panel/components/FeatureDescription.tsx
@@ -0,0 +1,20 @@
+import React, { type ReactNode } from "react";
+import { Text } from "@radix-ui/themes";
+import styled from "styled-components";
+
+interface Props {
+  children: ReactNode;
+}
+
+const DescriptionContainer = styled.div`
+    padding: var(--space-3);
+`;
+
+const FeatureDescription = ({ children }: Props) => {
+  return (
+    <DescriptionContainer>
+      <Text>{children}</Text>
+    </DescriptionContainer>
+  );
+};
+export default FeatureDescription;
