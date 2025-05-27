9716f47e62c1e14ff00a9300990e40591bd7f748
Mayaryin <lynn.janzen@posteo.de>
2025-05-27 13:10:18 +0200

    refactor: adapt header image and separator lines to dark mode

--- src/modules/feature-panel/components/LargeHeaderImage.tsx
+++ src/modules/feature-panel/components/LargeHeaderImage.tsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
-const LargeHeaderImage = styled.div`
+interface Props {
+  darkMode: boolean;
+}
+
+const LargeHeaderImage = styled.div<Props>`
     width: calc(100% + var(--space-3) * 2); 
     height: 15rem; 
     margin-left: calc(-1 * var(--space-3)); 
     margin-right: calc(-1 * var(--space-3)); 
     margin-bottom: var(--space-5);
-    background: lightgray;
+    background: ${(props) => (props.darkMode ? "darkgray" : "lightgray")};
     img { width: 100%; height: 100%; object-fit: cover; display: block; }
   `;
 
