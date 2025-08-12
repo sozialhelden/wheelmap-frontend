6e8092c7370c0e58a381c80b94cec0487e6ab9ce
Mayaryin <lynn.janzen@posteo.de>
2025-08-12 17:11:34 +0200

    feat: enable voice over access to radix' drop down component

--- src/needs-refactoring/components/CombinedFeaturePanel/components/AccessibilitySection/EditDropdownMenu.tsx
+++ src/needs-refactoring/components/CombinedFeaturePanel/components/AccessibilitySection/EditDropdownMenu.tsx
@@ -1,24 +1,56 @@
 import { DropdownMenu, IconButton } from "@radix-ui/themes";
 import { t } from "@transifex/native";
 import { Pencil } from "lucide-react";
-import React, { useState } from "react";
+import type React from "react";
+import { useRef, useState } from "react";
 import DescriptionEditor from "~/needs-refactoring/components/CombinedFeaturePanel/components/AccessibilitySection/DescriptionEditor";
 import type { AnyFeature } from "~/needs-refactoring/lib/model/geo/AnyFeature";
 
 type Props = {
   tagKey: string;
-  tagValue: string | undefined;
+  tagValue: string | undefined | number;
   feature: AnyFeature;
-};
-const EditDropdownMenu = ({ tagKey, tagValue, feature }: Props) => {
+} & React.ComponentPropsWithoutRef<"button">;
+
+const EditDropdownMenu = ({
+  tagKey,
+  tagValue,
+  feature,
+  ...triggerProps
+}: Props) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [addNewLanguage, setAddNewLanguage] = useState(false);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   return (
     <>
       <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
-          <IconButton variant="soft" size="2" data-testid={`${tagKey}`}>
+          <IconButton
+            ref={buttonRef}
+            {...triggerProps}
+            variant="soft"
+            size="2"
+            data-testid={`${tagKey}`}
+            aria-label={t("Edit description")}
+            onClick={(e) => {
+              triggerProps.onClick?.(e);
+
+              // VoiceOver’s “Press” / “Click” command fires a plain click event only.
+              // Radix DropdownMenu opens the menu on keydown or pointerdown and it does not react to a bare click.
+              // This, when a click happens, we manually dispatch a keyboard event
+
+              if (buttonRef.current) {
+                const keyEvent = new KeyboardEvent("keydown", {
+                  key: "Enter",
+                  bubbles: true,
+                  cancelable: true,
+                });
+                buttonRef.current.dispatchEvent(keyEvent);
+              }
+            }}
+          >
             <Pencil size={18} aria-hidden />
           </IconButton>
         </DropdownMenu.Trigger>
