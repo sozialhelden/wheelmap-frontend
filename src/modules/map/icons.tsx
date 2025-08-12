906e491e4c3f686336392fa418315baecdbb1613
Kris Siepert <m@kris.cool>
2025-08-12 12:21:08 +0200

    fix: use symbols instead of circles for dots
    This allows us to include polygons in the dots layer, thus including
    them in the list-view

--- src/modules/map/icons.tsx
+++ src/modules/map/icons.tsx
@@ -1,6 +1,14 @@
 import { type Category, getCategories } from "@sozialhelden/core";
 import type { ReactNode } from "react";
 
+import DotBadDark from "~/modules/map/icons/dots/dot-bad-dark.svg";
+import DotBad from "~/modules/map/icons/dots/dot-bad.svg";
+import DotGoodDark from "~/modules/map/icons/dots/dot-good-dark.svg";
+import DotGood from "~/modules/map/icons/dots/dot-good.svg";
+import DotMediocreDark from "~/modules/map/icons/dots/dot-mediocre-dark.svg";
+import DotMediocre from "~/modules/map/icons/dots/dot-mediocre.svg";
+import DotUnknownDark from "~/modules/map/icons/dots/dot-unknown-dark.svg";
+import DotUnknown from "~/modules/map/icons/dots/dot-unknown.svg";
 import ElevatorIcon from "~/modules/map/icons/elevators/elevator.svg";
 import BadEntranceIconDark from "~/modules/map/icons/entrances/house-entrance-bad-dark.svg";
 import BadEntranceIcon from "~/modules/map/icons/entrances/house-entrance-bad.svg";
@@ -37,6 +45,26 @@ export const icons: Record<string, MapIcon> = {
     type: "default",
     component: <ElevatorIcon />,
   },
+  "dot-good:": {
+    type: "default",
+    component: <DotGood />,
+    componentDarkMode: <DotGoodDark />,
+  },
+  "dot-mediocre:": {
+    type: "default",
+    component: <DotMediocre />,
+    componentDarkMode: <DotMediocreDark />,
+  },
+  "dot-bad:": {
+    type: "default",
+    component: <DotBad />,
+    componentDarkMode: <DotBadDark />,
+  },
+  "dot-unknown:": {
+    type: "default",
+    component: <DotUnknown />,
+    componentDarkMode: <DotUnknownDark />,
+  },
   "house-entrance-good": {
     type: "default",
     component: <GoodEntranceIcon />,
@@ -57,7 +85,6 @@ export const icons: Record<string, MapIcon> = {
     component: <UnknownEntranceIcon />,
     componentDarkMode: <UnknownEntranceIconDark />,
   },
-
   ...(["good", "mediocre", "bad", "unknown"] as AccessibilityGrade[]).reduce(
     (acc, accessibilityGrade) => {
       for (const category of Object.keys(getCategories()) as Category[]) {
