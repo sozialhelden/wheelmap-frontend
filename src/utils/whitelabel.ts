018fa55c9e53d871694af56ca7ff4675e697823a
Mayaryin <lynn.janzen@posteo.de>
2026-03-13 10:55:33 +0100

    fix: change hostname sanitation

--- src/utils/whitelabel.ts
+++ src/utils/whitelabel.ts
@@ -15,15 +15,15 @@ export function sanitizeHostname(hostnameOrIPAddress: string) {
     // Allow sharing a deployment with ngrok
     .replace(/.*\.ngrok(?:-free)\.app$/, "wheelmap.tech")
     // Allow branch test deployments with a common branding
-    .replace(/.*\.wheelmap\.tech$/, "wheelmap.tech");
+    .replace(/.*\.wheelmap\.tech$/, "wheelmap.tech")
+    // Use 'localhost' branding for loopback IPs
+    .replace(/127\.0\.0\.1/, "localhost")
+    .replace(/0\.0\.0\.0/, "localhost")
+    .replace(/::1/, "localhost");
 
   // Only replace local/private network IPs in development mode
   if (process.env.NODE_ENV === "development") {
     hostname = hostname
-      // Use 'localhost' branding for loopback IPs
-      .replace(/127\.0\.0\.1/, "localhost")
-      .replace(/0\.0\.0\.0/, "localhost")
-      .replace(/::1/, "localhost")
       // Use 'localhost' branding for private network IPs (192.168.x.x, 10.x.x.x, 172.16-31.x.x)
       .replace(/^192\.168\.\d{1,3}\.\d{1,3}$/, "localhost")
       .replace(/^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$/, "localhost")
