5119cc428413a48a7855ca15525f83256ed53d68
Christian Schade <christian@sozialhelden.de>
2026-03-24 12:55:09 +0100

    test written more easy to read

--- e2e/osm-map-label.e2e-spec.ts
+++ e2e/osm-map-label.e2e-spec.ts
@@ -5,12 +5,11 @@ import {
 import {
   mergeExpects,
   mergeTests,
-  expect as playwrightExpect,
   test as playwrightTest,
 } from "@playwright/test";
 
 const test = mergeTests(playwrightTest, mapGrabTest);
-const expect = mergeExpects(playwrightExpect, mapGrabExpect);
+const expect = mergeExpects(mapGrabExpect);
 
 test.use({
   launchOptions: {
@@ -22,9 +21,6 @@ test.use({
   },
 });
 
-// Zoom: 17.54018500
-// Center: [52.5170365,13.3888599]
-
 test("Supermarket should be visible on the map", async ({
   page,
   mapController,
@@ -33,27 +29,20 @@ test("Supermarket should be visible on the map", async ({
     waitUntil: "domcontentloaded",
   });
 
-  const isErrorPage = await page.evaluate(() => {
-    return (
-      location.pathname === "/_error" ||
-      (document.body?.textContent || "").includes('"statusCode":500')
-    );
-  });
-
-  expect(isErrorPage).toBe(false);
-
   await expect(
     page.locator(".mapboxgl-canvas, .maplibregl-canvas").first(),
   ).toBeVisible({ timeout: 15000 });
 
   await mapController("mainMap").waitToMapStable();
 
-  const shopSupermarketCount = await page.evaluate(() => {
-    const map = window.__MAPGRAB__.getMapInterface("mainMap").map;
-    return map.queryRenderedFeatures(undefined, {
-      filter: ["==", ["get", "shop"], "supermarket"],
-    }).length;
-  });
+  const count = await page.evaluate(
+    () =>
+      window.__MAPGRAB__
+        .getMapInterface("mainMap")
+        .map.queryRenderedFeatures(undefined, {
+          filter: ["==", ["get", "shop"], "supermarket"],
+        }).length,
+  );
 
-  expect(shopSupermarketCount).toBeGreaterThan(0);
+  expect(count).toBeGreaterThan(0);
 });
